42a8625f1bcec6737c0b1ac70c0b744a9513b4e3
Lukas <lukasstrassel@googlemail.com>
2022-08-04 16:42:40 +0200

    docs: improve docs (#7)

--- script/DeployL1Proposal.s.sol
+++ script/DeployL1Proposal.s.sol
@@ -5,13 +5,17 @@ import {Script} from 'forge-std/Script.sol';
 import {AaveGovernanceV2, IExecutorWithTimelock} from 'aave-address-book/AaveGovernanceV2.sol';
 
 contract DeployL1Proposal is Script {
-  // TODO: BRIDGE_EXECUTOR should be set once deployed
   address internal constant CROSSCHAIN_FORWARDER_POLYGON =
     address(0x158a6bC04F0828318821baE797f50B0A1299d45b);
+
+  // dynamic paramters
   address internal constant L2_PAYLOAD = address(0);
+  bytes32 internal constant IPFS_HASH = bytes32(0);
 
   function run() external {
     require(L2_PAYLOAD != address(0), "ERROR: L2_PAYLOAD can't be address(0)");
+    require(IPFS_HASH != bytes32(0), "ERROR: IPFS_HASH can't be bytes32(0)");
+
     vm.startBroadcast();
     address[] memory targets = new address[](1);
     targets[0] = CROSSCHAIN_FORWARDER_POLYGON;
@@ -30,7 +34,7 @@ contract DeployL1Proposal is Script {
       signatures,
       calldatas,
       withDelegatecalls,
-      bytes32(0)
+      IPFS_HASH
     );
     vm.stopBroadcast();
   }
