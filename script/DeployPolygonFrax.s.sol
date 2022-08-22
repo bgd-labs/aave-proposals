7aa578b60fdcca67ccf9dd168e36c4dc62f25c14
Lukas <lukasstrassel@googlemail.com>
2022-08-22 15:53:40 +0200

    docs: remove outdated comment

--- script/DeployPolygonFrax.s.sol
+++ script/DeployPolygonFrax.s.sol
@@ -4,11 +4,6 @@ pragma solidity ^0.8.0;
 import {Script} from 'forge-std/Script.sol';
 import {FraxPayload} from '../src/contracts/polygon/FraxPayload.sol';
 
-/**
- * to deploy copy .env.example to .env and fill it, and then execute:
- * source .env
- * forge script ./script/DeployPolygonFrax.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY --verify --etherscan-api-key $ETHERSCAN_API_KEY -vvvv
- */
 contract DeployPolygonFrax is Script {
   function run() external {
     vm.startBroadcast();
