8b1206819c2fa5be1c0975cfd81661c6f4a9dcf9
Lukas <lukasstrassel@googlemail.com>
2022-10-21 16:38:55 +0200

    feat: disable borrowing on dpi

--- src/contracts/polygon/RiskParameterUpdateV2.sol
+++ src/contracts/polygon/RiskParameterUpdateV2.sol
@@ -9,8 +9,12 @@ import {ProxyHelpers} from 'aave-helpers/ProxyHelpers.sol';
 contract RiskParameterUpdateV2 is IProposalGenericExecutor {
   address public constant GHST = 0x385Eeac5cB85A38A9a07A70c73e0a3271CfB54A7;
 
+  address public constant DPI = 0x85955046DF4668e1DD369D2DE9f3AEB98DD2A369;
+
   function execute() external override {
     // polygon v2
+    AaveV2Polygon.POOL_CONFIGURATOR.disableBorrowingOnReserve(DPI);
+
     AaveV2Polygon.POOL_CONFIGURATOR.disableBorrowingOnReserve(GHST);
     AaveV2Polygon.POOL_CONFIGURATOR.configureReserveAsCollateral(
       GHST,
