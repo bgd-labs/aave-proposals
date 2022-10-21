2e6045810ed448a5fcfb381def0c7fa8e8c10d98
Lukas <lukasstrassel@googlemail.com>
2022-10-21 16:51:04 +0200

    fix: remove unncessary imports

--- src/contracts/polygon/RiskParameterUpdateV2.sol
+++ src/contracts/polygon/RiskParameterUpdateV2.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
 import {AaveV2Polygon} from 'aave-address-book/AaveV2Polygon.sol';
 import {IProposalGenericExecutor} from '../../interfaces/IProposalGenericExecutor.sol';
-import {ProxyHelpers} from 'aave-helpers/ProxyHelpers.sol';
 
 contract RiskParameterUpdateV2 is IProposalGenericExecutor {
   address public constant GHST = 0x385Eeac5cB85A38A9a07A70c73e0a3271CfB54A7;
