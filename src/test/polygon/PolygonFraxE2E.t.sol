a79578d50e7802159dfb7997988b4cb0a2ce5669
Lukas <lukasstrassel@googlemail.com>
2022-12-05 10:38:20 +0100

    feat: simplify testing setup

--- src/test/polygon/PolygonFraxE2E.t.sol
+++ src/test/polygon/PolygonFraxE2E.t.sol
@@ -2,39 +2,25 @@
 pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
-import {AaveV3Polygon} from 'aave-address-book/AaveAddressBook.sol';
-import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveV3Polygon, AaveGovernanceV2} from 'aave-address-book/AaveAddressBook.sol';
 import {ProtocolV3TestBase, ReserveConfig, ReserveTokens, IERC20} from 'aave-helpers/ProtocolV3TestBase.sol';
-import {BridgeExecutorHelpers} from 'aave-helpers/BridgeExecutorHelpers.sol';
 import {AaveGovernanceV2, IExecutorWithTimelock} from 'aave-address-book/AaveGovernanceV2.sol';
-import {IStateReceiver} from 'governance-crosschain-bridges/contracts/dependencies/polygon/fxportal/FxChild.sol';
-import {CrosschainForwarderPolygon} from '../../contracts/polygon/CrosschainForwarderPolygon.sol';
 import {FraxPayload} from '../../contracts/polygon/FraxPayload.sol';
-import {DeployL1PolygonProposal} from '../../../script/DeployL1PolygonProposal.s.sol';
+import {BaseTest} from '../utils/BaseTest.sol';
 
-contract PolygonFraxE2ETest is ProtocolV3TestBase {
+contract PolygonFraxE2ETest is ProtocolV3TestBase, BaseTest {
   // the identifiers of the forks
-  uint256 mainnetFork;
   uint256 polygonFork;
 
   FraxPayload public fraxPayload;
-
-  address public constant CROSSCHAIN_FORWARDER_POLYGON =
-    0x158a6bC04F0828318821baE797f50B0A1299d45b;
-  address public constant BRIDGE_ADMIN =
-    0x0000000000000000000000000000000000001001;
-  address public constant FX_CHILD_ADDRESS =
-    0x8397259c983751DAf40400790063935a11afa28a;
-  address public constant POLYGON_BRIDGE_EXECUTOR =
-    0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772;
 
   address public constant FRAX = 0x45c32fA6DF82ead1e2EF74d17b76547EDdFaFF89;
 
   address public constant DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
 
   function setUp() public {
-    polygonFork = vm.createFork(vm.rpcUrl('polygon'), 31507646);
-    mainnetFork = vm.createFork(vm.rpcUrl('ethereum'), 15275388);
+    polygonFork = vm.createSelectFork(vm.rpcUrl('polygon'), 31507646);
+    _setUp(AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR);
   }
 
   function testProposalE2E() public {
@@ -48,39 +34,10 @@ contract PolygonFraxE2ETest is ProtocolV3TestBase {
     // 1. deploy l2 payload
     fraxPayload = new FraxPayload();
 
-    // 2. create l1 proposal
-    vm.selectFork(mainnetFork);
-    vm.startPrank(GovHelpers.AAVE_WHALE);
-    uint256 proposalId = DeployL1PolygonProposal._deployL1Proposal(
-      address(fraxPayload),
-      0xec9d2289ab7db9bfbf2b0f2dd41ccdc0a4003e9e0d09e40dee09095145c63fb5 // TODO: replace with actual ipfs-hash
-    );
-    vm.stopPrank();
+    // 2. execute l2 payload
+    _execute(address(fraxPayload));
 
-    // 3. execute proposal and record logs so we can extract the emitted StateSynced event
-    vm.recordLogs();
-    GovHelpers.passVoteAndExecute(vm, proposalId);
-
-    Vm.Log[] memory entries = vm.getRecordedLogs();
-    assertEq(
-      keccak256('StateSynced(uint256,address,bytes)'),
-      entries[2].topics[0]
-    );
-    assertEq(address(uint160(uint256(entries[2].topics[2]))), FX_CHILD_ADDRESS);
-
-    // 4. mock the receive on l2 with the data emitted on StateSynced
-    vm.selectFork(polygonFork);
-    vm.startPrank(BRIDGE_ADMIN);
-    IStateReceiver(FX_CHILD_ADDRESS).onStateReceive(
-      uint256(entries[2].topics[1]),
-      this._cutBytes(entries[2].data)
-    );
-    vm.stopPrank();
-
-    // 5. Forward time & execute proposal
-    BridgeExecutorHelpers.waitAndExecuteLatest(vm, POLYGON_BRIDGE_EXECUTOR);
-
-    // 6. verify results
+    // 3. verify results
     ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(
       AaveV3Polygon.POOL
     );
