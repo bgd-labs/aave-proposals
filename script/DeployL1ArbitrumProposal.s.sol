0390fcf539761f57e286776924c4727bf205c3a5
Lukas <lukasstrassel@googlemail.com>
2022-12-07 12:19:52 +0100

    fix: op risk changes (#29)
    * fix: op risk changes
    
    * fix: remove change [skip ci]
    
    * fix: update op to latest params
    
    * fix: update tests
    
    * fix: let stETH do sth

--- script/DeployL1ArbitrumProposal.s.sol
+++ script/DeployL1ArbitrumProposal.s.sol
@@ -6,18 +6,14 @@ import {Script} from 'forge-std/Script.sol';
 import {AaveGovernanceV2, IExecutorWithTimelock} from 'aave-address-book/AaveGovernanceV2.sol';
 
 library DeployL1ArbitrumProposal {
-  // address internal constant CROSSCHAIN_FORWARDER_ARBITRUM =
-  //   address(0x158a6bC04F0828318821baE797f50B0A1299d45b);
-
-  function _deployL1Proposal(
-    address payload,
-    bytes32 ipfsHash,
-    address crosschainForwarderArbitrum
-  ) internal returns (uint256 proposalId) {
+  function _deployL1Proposal(address payload, bytes32 ipfsHash)
+    internal
+    returns (uint256 proposalId)
+  {
     require(payload != address(0), "ERROR: L2_PAYLOAD can't be address(0)");
     require(ipfsHash != bytes32(0), "ERROR: IPFS_HASH can't be bytes32(0)");
     address[] memory targets = new address[](1);
-    targets[0] = crosschainForwarderArbitrum; // CROSSCHAIN_FORWARDER_ARBITRUM;
+    targets[0] = AaveGovernanceV2.CROSSCHAIN_FORWARDER_ARBITRUM;
     uint256[] memory values = new uint256[](1);
     values[0] = 0;
     string[] memory signatures = new string[](1);
@@ -43,11 +39,7 @@ library DeployL1ArbitrumProposal {
 contract DeployStEth is Script {
   function run() external {
     vm.startBroadcast();
-    DeployL1ArbitrumProposal._deployL1Proposal(
-      address(0),
-      bytes32(0),
-      address(0)
-    );
+    DeployL1ArbitrumProposal._deployL1Proposal(address(0), bytes32(0));
     vm.stopBroadcast();
   }
 }
