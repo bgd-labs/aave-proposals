9a5cfadfc619b168fa6d7648fb195b81f9b99c10
Lukas <lukasstrassel@googlemail.com>
2023-02-02 11:12:59 +0100

    fix: update cbETH example

--- script/CreateMainnetProposals.s.sol
+++ script/CreateMainnetProposals.s.sol
@@ -2,44 +2,17 @@
 pragma solidity ^0.8.0;
 
 import {Script} from 'forge-std/Script.sol';
-import {AaveGovernanceV2, IExecutorWithTimelock} from 'aave-address-book/AaveGovernanceV2.sol';
-
-library DeployL1Proposal {
-  function _deployL1Proposal(address payload, bytes32 ipfsHash)
-    internal
-    returns (uint256 proposalId)
-  {
-    require(payload != address(0), "ERROR: PAYLOAD can't be address(0)");
-    require(ipfsHash != bytes32(0), "ERROR: IPFS_HASH can't be bytes32(0)");
-    address[] memory targets = new address[](1);
-    targets[0] = payload;
-    uint256[] memory values = new uint256[](1);
-    values[0] = 0;
-    string[] memory signatures = new string[](1);
-    signatures[0] = 'execute()';
-    bytes[] memory calldatas = new bytes[](1);
-    calldatas[0] = '';
-    bool[] memory withDelegatecalls = new bool[](1);
-    withDelegatecalls[0] = true;
-
-    return
-      AaveGovernanceV2.GOV.create(
-        IExecutorWithTimelock(AaveGovernanceV2.SHORT_EXECUTOR),
-        targets,
-        values,
-        signatures,
-        calldatas,
-        withDelegatecalls,
-        ipfsHash
-      );
-  }
-}
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 
 contract CreateCbETHProposal is Script {
   function run() external {
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](2);
+    payloads[0] = GovHelpers.buildMainnet(
+      0xd91d1331db4F436DaF47Ec9Dd86deCb8EEF946B4
+    );
     vm.startBroadcast();
-    DeployL1Proposal._deployL1Proposal(
-      0xd91d1331db4F436DaF47Ec9Dd86deCb8EEF946B4,
+    GovHelpers.createProposal(
+      payloads,
       0x05097b8a0818a75c1db7d54dfd0299581cac0218a058017acb4726f7cc49657e
     );
     vm.stopBroadcast();
