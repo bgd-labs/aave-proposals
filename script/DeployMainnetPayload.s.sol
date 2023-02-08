5d39671c91b61fc29a8488125797cbcb0654a4bd
Rustem Kurmaev <rustboyar@gmail.com>
2023-02-08 17:26:01 +0300

    Fix: r eth deployment script make command (#45)
    * Script to deploy rETH payload
    
    * make command to deploy rETH
    
    * broadcast flag added

--- script/DeployMainnetPayload.s.sol
+++ script/DeployMainnetPayload.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import {WithChainIdValidation} from './WithChainIdValidation.sol';
 
 // import {AaveV3EthcbETHPayload} from '../src/contracts/mainnet/AaveV3EthcbETHPayload.sol';
+import {AaveV3EthrETHPayload} from '../src/contracts/mainnet/AaveV3EthrETHPayload.sol';
 
 contract DeployMainnetPayload is WithChainIdValidation {
   constructor() WithChainIdValidation(1) {}
@@ -19,3 +20,11 @@ contract DeployMainnetPayload is WithChainIdValidation {
 //     vm.stopBroadcast();
 //   }
 // }
+
+contract rETH is DeployMainnetPayload {
+  function run() external {
+    vm.startBroadcast();
+    new AaveV3EthrETHPayload();
+    vm.stopBroadcast();
+  }
+}
