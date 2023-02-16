a120b060e0622a354c0cd15db93c3d9324b14d64
Lukas <lukasstrassel@googlemail.com>
2023-02-16 12:03:36 +0100

    fix: add pr template

---
+++ .github/workflows/pull_request_template.md
@@ -0,0 +1,9 @@
+## AIP checklist
+
+- [ ] payload contains header comment linking discussion & snapshot
+- [ ] a test covering the changes is included
+- [ ] new scripts are included
+
+## References
+
+- [AIP text](link to aip repo)
--- script/CreateMainnetProposals.s.sol
+++ script/CreateMainnetProposals.s.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
+// ################ EXAMPLE ################
+// PLEASE COPY THE FILE & ADJUST ACCORDINGLY
+//    WE WILL REMOVE SCRIPTS PERIODICALLY
+// #########################################
 pragma solidity ^0.8.0;
 import {WithChainIdValidation} from './WithChainIdValidation.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
