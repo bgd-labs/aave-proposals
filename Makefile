ed81d3a8c3a779aa2eb4d148222d821757bbefa7
efecarranza <fcarr004@fiu.edu>
2023-02-15 20:08:39 -0500

    feat: add script for strategy

--- Makefile
+++ Makefile
@@ -54,3 +54,5 @@ reth-payload :;  forge script script/DeployMainnetPayload.s.sol:rETH --rpc-url m
 
 xsushi-feed-payload :;  forge script script/DeployMainnetPayload.s.sol:AaveV2SwapxSushiOracle --rpc-url mainnet --broadcast --legacy --ledger --mnemonic-indexes ${MNEMONIC_INDEX} --sender ${LEDGER_SENDER} --verify --etherscan-api-key ${ETHERSCAN_API_KEY_MAINNET} -vvvv
 xsushi-feed-proposal :; forge script script/CreateMainnetProposals.s.sol:SwapXSushiPriceFeedPayloadProposal --rpc-url mainnet --broadcast --legacy --ledger --mnemonic-indexes ${MNEMONIC_INDEX} --sender ${LEDGER_SENDER} -vvvv
+
+deploy-contract :; forge script script/DeployContract.s.sol:DeployContract --rpc-url ${RPC_MAINNET_URL} --broadcast --private-key ${PRIVATE_KEY} --verify --etherscan-api-key ${ETHERSCAN_API_KEY} -vvvv
--- src/test/optimism/AaveV3OptWSTETHListingPayloadTest.t.sol
+++ src/test/optimism/AaveV3OptWSTETHListingPayloadTest.t.sol
@@ -7,4 +7,23 @@ import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
 import {AaveV3OptWSTETHListingPayload} from '../../contracts/optimism/AaveV3OptWSTETHListingPayload.sol';
 
-contract AaveV3OptWSTETHListingPayloadTest is ProtocolV3TestBase, TestWithExecutor {}
+contract AaveV3OptWSTETHListingPayloadTest is ProtocolV3TestBase, TestWithExecutor {
+  AaveV3OptWSTETHListingPayload public payload;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('optimism'), 16582764);
+    _selectPayloadExecutor(AaveGovernanceV2.OPTIMISM_BRIDGE_EXECUTOR);
+
+    payload = new AaveV3OptWSTETHListingPayload();
+  }
+
+  function testPoolActivation() public {
+    createConfigurationSnapshot('pre-wstETH-Aave-V3-Optimism', AaveV3Optimism.POOL);
+
+    _executePayload(address(payload));
+
+    createConfigurationSnapshot('post-wstETH-Aave-V3-Optimism', AaveV3Optimism.POOL);
+
+    diffReports('pre-wstETH-Aave-V3-Optimism', 'post-wstETH-Aave-V3-Optimism');
+  }
+}
