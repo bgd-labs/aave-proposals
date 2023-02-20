839e73b1f43fad90f77149dd4cfd24316fabb7d6
marczeller <pierarte@gmail.com>
2023-02-20 18:47:28 +0100

    create BUSd AIP

---
+++ script/CreateBUSDFreeze.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import {WithChainIdValidation} from './WithChainIdValidation.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+
+contract CreateMainnetProposal is WithChainIdValidation {
+  constructor() WithChainIdValidation(1) {}
+}
+
+contract FreezeBUSDProposal is CreateMainnetProposal {
+  bytes32 internal IPFS_HASH = 0; // TOFIX: Replace with actual hash
+
+  function run() external {
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
+    payloads[0] = GovHelpers.buildMainnet(
+      0 // TOFIX: Replace with actual address
+    );
+    vm.startBroadcast();
+    GovHelpers.createProposal(
+      payloads,
+      IPFS_HASH // TOFIX: Replace with actual hash
+    );
+    vm.stopBroadcast();
+  }
+}
