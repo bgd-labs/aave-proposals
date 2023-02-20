19879f8a60142dd0082bd9de0a35714eb3fe19ce
marczeller <pierarte@gmail.com>
2023-02-20 17:48:15 +0100

    final create script with IPFS hash

--- script/CreateCBETHCapProposal.s.sol
+++ script/CreateCBETHCapProposal.s.sol
@@ -8,7 +8,7 @@ contract CreateMainnetProposal is WithChainIdValidation {
 }
 
 contract CBETHCAPProposal is CreateMainnetProposal {
-  bytes32 internal IPFS_HASH = 0; // TOFIX: Replace with actual hash
+  bytes32 internal IPFS_HASH = 0xf7ac3c7b7252b6ca6ee17efb10f9f31c9f22bfb737573085b3e57d89456be123;
 
   function run() external {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
@@ -18,7 +18,7 @@ contract CBETHCAPProposal is CreateMainnetProposal {
     vm.startBroadcast();
     GovHelpers.createProposal(
       payloads,
-      IPFS_HASH // TOFIX: Replace with actual hash
+      IPFS_HASH
     );
     vm.stopBroadcast();
   }
