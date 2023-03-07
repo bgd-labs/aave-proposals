16268581d068323b73e93f01e53d91537792e8f1
Marc Zeller <pierarte@gmail.com>
2023-03-07 14:48:03 +0100

    Update src/contracts/mainnet/AaveV3EthCBETHSupplyCapsPayload.sol
    Co-authored-by: Lukas <lukasstrassel@googlemail.com>

--- src/contracts/mainnet/AaveV3EthCBETHSupplyCapsPayload.sol
+++ src/contracts/mainnet/AaveV3EthCBETHSupplyCapsPayload.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.16;
 
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 import {IPoolConfigurator, ConfiguratorInputTypes} from 'aave-address-book/AaveV3.sol';
