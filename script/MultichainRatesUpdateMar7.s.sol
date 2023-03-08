4778378360769aa433a848e1898b453e8a5eb0a6
eboado <ebdmrr@gmail.com>
2023-03-08 16:38:03 +0100

    - Added deployed payloads to script of Gauntlet rates recommendations

--- script/MultichainRatesUpdateMar7.s.sol
+++ script/MultichainRatesUpdateMar7.s.sol
@@ -9,15 +9,9 @@ import 'aave-helpers/../script/Utils.s.sol';
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](3);
-    payloads[0] = GovHelpers.buildPolygon(
-      address(0) // TODO add payload address
-    );
-    payloads[1] = GovHelpers.buildOptimism(
-      address(0) // TODO add payload address
-    );
-    payloads[2] = GovHelpers.buildArbitrum(
-      address(0) // TODO add payload address
-    );
+    payloads[0] = GovHelpers.buildPolygon(0xDBe6EB920ca5a79ae2151b6DCbd2c1Fa3A19aD1a);
+    payloads[1] = GovHelpers.buildOptimism(0x7902F3c60f05b5A6b7e4Ce0Cac11Cb17bC8e607c);
+    payloads[2] = GovHelpers.buildArbitrum(0x7A9A9c14B35E58ffa1cC84aB421acE0FdcD289E3);
     GovHelpers.createProposal(
       payloads,
       0x3b66ed0fa57bec842608cb1be68f97ca0c241cd3744a860086762f76cc0ae933,
