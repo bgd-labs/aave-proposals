c12b086f61aed3cac018b939138dd506a7319cc9
altmark <michala@chaoslabs.xyz>
2023-03-08 18:53:48 +0200

    fix comments

--- src/test/mainnet/AaveV3EthParamsPayload-Mar02Test.t.sol
+++ src/test/mainnet/AaveV3EthParamsPayload-Mar02Test.t.sol
@@ -1,20 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.16;
 
 import 'forge-std/Test.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveAddressBook.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
-import {ProtocolV3TestBase, ReserveConfig, ReserveTokens, IERC20} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3EthParamsPayload} from '../../contracts/mainnet/AaveV3EthParamsPayload-Mar02.sol';
 import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
 
 contract AaveV3EthParamsPayloadTest is ProtocolV3TestBase, TestWithExecutor {
   AaveV3EthParamsPayload public proposalPayload;
-
-  address public constant USDC = AaveV3EthereumAssets.USDC_UNDERLYING;
-  address public constant DAI = AaveV3EthereumAssets.DAI_UNDERLYING;
-
 
   uint256 public constant USDC_LT = 7900; // 79%
   uint256 public constant USDC_LTV = 7700; // 77%
@@ -44,13 +39,13 @@ contract AaveV3EthParamsPayloadTest is ProtocolV3TestBase, TestWithExecutor {
     );
 
     // USDC
-    ReserveConfig memory USDCConfig = ProtocolV3TestBase._findReserveConfig(allConfigsBefore, USDC);
+    ReserveConfig memory USDCConfig = ProtocolV3TestBase._findReserveConfig(allConfigsBefore, AaveV3EthereumAssets.USDC_UNDERLYING);
     USDCConfig.liquidationThreshold = USDC_LT;
     USDCConfig.ltv = USDC_LTV;
     ProtocolV3TestBase._validateReserveConfig(USDCConfig, allConfigsAfter);
 
     // DAI
-    ReserveConfig memory DAIConfig = ProtocolV3TestBase._findReserveConfig(allConfigsBefore, DAI);
+    ReserveConfig memory DAIConfig = ProtocolV3TestBase._findReserveConfig(allConfigsBefore, AaveV3EthereumAssets.DAI_UNDERLYING);
     DAIConfig.liquidationThreshold = DAI_LT;
     DAIConfig.ltv = DAI_LTV;
     ProtocolV3TestBase._validateReserveConfig(DAIConfig, allConfigsAfter);
