811511ab9354c38f1de3395c1e03151afb171a95
yonikesel <yonikesel@chaoslabs.xyz>
2023-03-17 16:55:19 +0200

    combine payloads

--- src/test/polygon/AaveV3PolCapsPayloadTest-Mar17.t.sol
+++ src/test/polygon/AaveV3PolCapsPayloadTest-Mar17.t.sol
@@ -5,12 +5,14 @@ import 'forge-std/Test.sol';
 import {AaveGovernanceV2, AaveV3Polygon} from 'aave-address-book/AaveAddressBook.sol';
 import {AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
-import {AaveV3PolSTMATICCapPayload} from '../../contracts/polygon/AaveV3PolSTMATICCapPayload-Mar17.sol';
+import {AaveV3PolCapsPayload} from '../../contracts/polygon/AaveV3PolCapsPayload-Mar17.sol';
 import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
 
-contract AaveV3PolSTMATICCapPayloadTest is ProtocolV3TestBase, TestWithExecutor {
-  AaveV3PolSTMATICCapPayload public proposalPayload;
+contract AaveV3PolCapsPayloadTest is ProtocolV3TestBase, TestWithExecutor {
+  AaveV3PolCapsPayload public proposalPayload;
 
+  uint256 public constant MAI_SUPPLY_CAP = 2_200_000;
+  uint256 public constant MAI_BORROW_CAP = 1_200_000;
   uint256 public constant STMATIC_SUPPLY_CAP = 21_000_000;
 
   function setUp() public {
@@ -28,8 +30,16 @@ contract AaveV3PolSTMATICCapPayloadTest is ProtocolV3TestBase, TestWithExecutor 
     );
     STMATICConfig.supplyCap = STMATIC_SUPPLY_CAP;
 
+    //MAI
+    ReserveConfig memory MAIConfig = ProtocolV3TestBase._findReserveConfig(
+      allConfigsBefore,
+      AaveV3PolygonAssets.miMATIC_UNDERLYING
+    );
+    MAIConfig.supplyCap = MAI_SUPPLY_CAP;
+    MAIConfig.borrowCap = MAI_BORROW_CAP;
+
     // 1. deploy l2 payload
-    proposalPayload = new AaveV3PolSTMATICCapPayload();
+    proposalPayload = new AaveV3PolCapsPayload();
 
     // 2. execute l2 payload
     _executePayload(address(proposalPayload));
@@ -38,5 +48,6 @@ contract AaveV3PolSTMATICCapPayloadTest is ProtocolV3TestBase, TestWithExecutor 
     ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(AaveV3Polygon.POOL);
 
     _validateReserveConfig(STMATICConfig, allConfigsAfter);
+    _validateReserveConfig(MAIConfig, allConfigsAfter);
   }
 }
