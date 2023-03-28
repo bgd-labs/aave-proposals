efaf5ee9a274268ed669590901c5ea21932def22
efecarranza <fcarr004@fiu.edu>
2023-03-28 10:35:46 -0400

    add comments

--- src/AaveV3CapsUpdates_20230328/AaveV3PolCapsUpdates_20230328.sol
+++ src/AaveV3CapsUpdates_20230328/AaveV3PolCapsUpdates_20230328.sol
@@ -5,6 +5,13 @@ pragma solidity 0.8.17;
 import {AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
 import {AaveV3PayloadPolygon, IEngine, EngineFlags} from 'aave-helpers/v3-config-engine/AaveV3PayloadPolygon.sol';
 
+/**
+ * @title AaveV3PolCapsUpdates_20230328
+ * @author Llama
+ * @dev Update the Aave V3 Polygon wMATIC supply cap
+ * Governance Forum Post: https://governance.aave.com/t/arfc-wmatic-supply-cap-increase/12046
+ * Snapshot: https://snapshot.org/#/aave.eth/proposal/0xb4c756ac701b5cbc0ec2d832652e4f1c820aa40f7d9be18eb81f87080a52c5f1
+ */
 contract AaveV3PolCapsUpdates_20230328 is AaveV3PayloadPolygon {
   uint256 public constant NEW_SUPPLY_CAP = 57_000_000;
 
