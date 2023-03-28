52bd92967ba7cc56f317bb3f055fabd1cd9cfb92
Lukas <lukasstrassel@googlemail.com>
2023-03-28 11:46:10 +0200

    fix: update examples

--- script/DeployPolygonPayload.s.sol
+++ script/DeployPolygonPayload.s.sol
@@ -1,21 +1,13 @@
-// // SPDX-License-Identifier: MIT
-// // ################ EXAMPLE ################
-// // PLEASE COPY THE FILE & ADJUST ACCORDINGLY
-// //    WE WILL REMOVE SCRIPTS PERIODICALLY
-// // #########################################
-// pragma solidity ^0.8.0;
-// import {WithChainIdValidation} from './WithChainIdValidation.sol';
+// SPDX-License-Identifier: MIT
+// ################ EXAMPLE ################
+// PLEASE COPY THE FILE & ADJUST ACCORDINGLY
+//    WE WILL REMOVE SCRIPTS PERIODICALLY
+// #########################################
+pragma solidity ^0.8.0;
+import {PolygonScript} from 'aave-helpers/../script/Utils.s.sol';
 
-// import {MiMaticPayload} from '../src/contracts/polygon/MiMaticPayload.sol';
-
-// contract DeployPolygonPayload is WithChainIdValidation {
-//   constructor() WithChainIdValidation(137) {}
-// }
-
-// contract MiMatic is DeployPolygonPayload {
-//   function run() external {
-//     vm.startBroadcast();
-//     new MiMaticPayload();
-//     vm.stopBroadcast();
-//   }
-// }
+contract ExamplePolygonPayload is PolygonScript {
+  function run() external broadcast {
+    // new PolygonPayload();
+  }
+}
