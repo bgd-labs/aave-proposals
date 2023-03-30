75f0d6dd2ded4858f8e40daac56d0d5b14798556
altmark <michala@chaoslabs.xyz>
2023-03-30 18:45:25 +0300

    Prettier and author

--- src/AaveV3ETHIsoMode_20230330/AaveV3ETHIsoMode_20230330.sol
+++ src/AaveV3ETHIsoMode_20230330/AaveV3ETHIsoMode_20230330.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.16;
 import 'aave-helpers/v3-config-engine/AaveV3PayloadEthereum.sol';
 
 /**
-* @dev change 4 stable asset to be in borrowable isolation mode - usdc, usdt, dai and lusd
-* - Discussion: https://governance.aave.com/t/arfc-configure-isolation-mode-borrowable-assets-v3-ethereum/12420
-*/
+ * @dev change 4 stable asset to be in borrowable isolation mode - usdc, usdt, dai and lusd
+ * @author Chaos Labs
+ * - Discussion: https://governance.aave.com/t/arfc-configure-isolation-mode-borrowable-assets-v3-ethereum/12420
+ */
 contract AaveV3ETHIsoMode_20230330 is AaveV3PayloadEthereum {
-
   function borrowsUpdates() public pure override returns (IEngine.BorrowUpdate[] memory) {
     IEngine.BorrowUpdate[] memory borrowsUpdate = new IEngine.BorrowUpdate[](4);
 
@@ -20,7 +20,7 @@ contract AaveV3ETHIsoMode_20230330 is AaveV3PayloadEthereum {
       borrowableInIsolation: EngineFlags.ENABLED,
       withSiloedBorrowing: EngineFlags.KEEP_CURRENT
     });
-    
+
     borrowsUpdate[1] = IEngine.BorrowUpdate({
       asset: AaveV3EthereumAssets.USDT_UNDERLYING,
       reserveFactor: EngineFlags.KEEP_CURRENT,
@@ -30,7 +30,7 @@ contract AaveV3ETHIsoMode_20230330 is AaveV3PayloadEthereum {
       borrowableInIsolation: EngineFlags.ENABLED,
       withSiloedBorrowing: EngineFlags.KEEP_CURRENT
     });
-        
+
     borrowsUpdate[2] = IEngine.BorrowUpdate({
       asset: AaveV3EthereumAssets.DAI_UNDERLYING,
       reserveFactor: EngineFlags.KEEP_CURRENT,
@@ -40,7 +40,7 @@ contract AaveV3ETHIsoMode_20230330 is AaveV3PayloadEthereum {
       borrowableInIsolation: EngineFlags.ENABLED,
       withSiloedBorrowing: EngineFlags.KEEP_CURRENT
     });
-        
+
     borrowsUpdate[3] = IEngine.BorrowUpdate({
       asset: AaveV3EthereumAssets.LUSD_UNDERLYING,
       reserveFactor: EngineFlags.KEEP_CURRENT,
@@ -51,5 +51,5 @@ contract AaveV3ETHIsoMode_20230330 is AaveV3PayloadEthereum {
       withSiloedBorrowing: EngineFlags.KEEP_CURRENT
     });
     return borrowsUpdate;
-    }
+  }
 }
--- src/AaveV3ETHIsoMode_20230330/AaveV3ETHIsoMode_20230330_Test.t.sol
+++ src/AaveV3ETHIsoMode_20230330/AaveV3ETHIsoMode_20230330_Test.t.sol
@@ -10,72 +10,61 @@ import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 
 contract AaveV3ETHIsoMode_20230330_Test is ProtocolV3TestBase, TestWithExecutor {
-    function setUp() public {
-        vm.createSelectFork(vm.rpcUrl('mainnet'), 16925078);
-        _selectPayloadExecutor(AaveGovernanceV2.SHORT_EXECUTOR);
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 16925078);
+    _selectPayloadExecutor(AaveGovernanceV2.SHORT_EXECUTOR);
+  }
 
-    }
+  function testPayload() public {
+    AaveV3ETHIsoMode_20230330 proposalPayload = new AaveV3ETHIsoMode_20230330();
 
-    function testPayload() public {
-        AaveV3ETHIsoMode_20230330 proposalPayload = new AaveV3ETHIsoMode_20230330();
+    ReserveConfig[] memory allConfigsBefore = _getReservesConfigs(AaveV3Ethereum.POOL);
 
-        ReserveConfig[] memory allConfigsBefore = _getReservesConfigs(AaveV3Ethereum.POOL);
+    // 1. create snapshot before payload execution
+    createConfigurationSnapshot('preAaveV3ETHIsoMode_20230330_Change', AaveV3Ethereum.POOL);
 
-        // 1. create snapshot before payload execution
-        createConfigurationSnapshot(
-        'preAaveV3ETHIsoMode_20230330_Change',
-        AaveV3Ethereum.POOL
-        );
+    // 2. execute payload
+    _executePayload(address(proposalPayload));
 
-        // 2. execute payload
-        _executePayload(address(proposalPayload));
+    // 3. create snapshot before payload execution
+    createConfigurationSnapshot('postAaveV3ETHIsoMode_20230330_Change', AaveV3Ethereum.POOL);
 
-        // 3. create snapshot before payload execution
-        createConfigurationSnapshot(
-        'postAaveV3ETHIsoMode_20230330_Change',
-        AaveV3Ethereum.POOL
-        );
+    // 4. Verify payload:
+    ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(AaveV3Ethereum.POOL);
 
-        // 4. Verify payload:
-        ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(AaveV3Ethereum.POOL);
-        
+    ReserveConfig memory USDC_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
+      allConfigsBefore,
+      AaveV3EthereumAssets.USDC_UNDERLYING
+    );
 
-        ReserveConfig memory USDC_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
-        allConfigsBefore,
-        AaveV3EthereumAssets.USDC_UNDERLYING
-        );
-    
-        USDC_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
-        ProtocolV3TestBase._validateReserveConfig(USDC_UNDERLYING_CONFIG, allConfigsAfter);
+    USDC_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
+    ProtocolV3TestBase._validateReserveConfig(USDC_UNDERLYING_CONFIG, allConfigsAfter);
 
-        ReserveConfig memory USDT_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
-        allConfigsBefore,
-        AaveV3EthereumAssets.USDT_UNDERLYING
-        );
-        
-        USDT_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
-        ProtocolV3TestBase._validateReserveConfig(USDT_UNDERLYING_CONFIG, allConfigsAfter);
+    ReserveConfig memory USDT_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
+      allConfigsBefore,
+      AaveV3EthereumAssets.USDT_UNDERLYING
+    );
 
-        ReserveConfig memory DAI_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
-        allConfigsBefore,
-        AaveV3EthereumAssets.DAI_UNDERLYING
-        );
-        
-        DAI_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
-        ProtocolV3TestBase._validateReserveConfig(DAI_UNDERLYING_CONFIG, allConfigsAfter);
+    USDT_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
+    ProtocolV3TestBase._validateReserveConfig(USDT_UNDERLYING_CONFIG, allConfigsAfter);
 
-        ReserveConfig memory LUSD_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
-        allConfigsBefore,
-        AaveV3EthereumAssets.LUSD_UNDERLYING
-        );
-        
-        LUSD_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
-        ProtocolV3TestBase._validateReserveConfig(LUSD_UNDERLYING_CONFIG, allConfigsAfter);
+    ReserveConfig memory DAI_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
+      allConfigsBefore,
+      AaveV3EthereumAssets.DAI_UNDERLYING
+    );
 
-        // 5. compare snapshots
-        diffReports(
-            'preAaveV3ETHIsoMode_20230330_Change',
-            'postAaveV3ETHIsoMode_20230330_Change'
-        );
-    }
+    DAI_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
+    ProtocolV3TestBase._validateReserveConfig(DAI_UNDERLYING_CONFIG, allConfigsAfter);
+
+    ReserveConfig memory LUSD_UNDERLYING_CONFIG = ProtocolV3TestBase._findReserveConfig(
+      allConfigsBefore,
+      AaveV3EthereumAssets.LUSD_UNDERLYING
+    );
+
+    LUSD_UNDERLYING_CONFIG.isBorrowableInIsolation = true;
+    ProtocolV3TestBase._validateReserveConfig(LUSD_UNDERLYING_CONFIG, allConfigsAfter);
+
+    // 5. compare snapshots
+    diffReports('preAaveV3ETHIsoMode_20230330_Change', 'postAaveV3ETHIsoMode_20230330_Change');
+  }
 }
