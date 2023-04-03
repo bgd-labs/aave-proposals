57630099f99a4508077711fce803ded90b5937ad
Lukas <lukasstrassel@googlemail.com>
2023-04-03 20:04:12 +0200

    fix: better imports

--- src/AaveV3DFSFlashBorrow_20230403/AaveV3ARBDFSFlashBorrowActivation_20230403_test.t.sol
+++ src/AaveV3DFSFlashBorrow_20230403/AaveV3ARBDFSFlashBorrowActivation_20230403_test.t.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.16;
 import 'forge-std/Test.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveAddressBook.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
-import {ProtocolV3TestBase, ReserveConfig, ReserveTokens, IERC20} from 'aave-helpers/ProtocolV3TestBase.sol';
-import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {IERC20} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3EthDFSFlashBorrowActivation} from './AaveV3ETHDFSFlashBorrowActivation_20230403.sol';
 import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
 
@@ -29,7 +28,7 @@ contract MockReceiver {
 }
 
 contract AaveV3EthDFSFlashBorrowActivationTest is TestWithExecutor, MockReceiver {
-  address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+  address internal constant DAI = AaveV3EthereumAssets.DAI_UNDERLYING;
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 16932117);
