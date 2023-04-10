639538d2bf9398b65290d4550158a443c175e79c
Jonathan Reem <jonathan.reem@gmail.com>
2023-04-10 20:37:27 -0300

    Add deployed payload and ipfs hash.

--- src/AaveV3ArbBorrowCapsUpdate_20230410/DeployAaveV3ArbBorrowCapsUpdate_20230410.s.sol
+++ src/AaveV3ArbBorrowCapsUpdate_20230410/DeployAaveV3ArbBorrowCapsUpdate_20230410.s.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.8.16;
 
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
-import {ArbitrumScript} from 'aave-helpers/../script/Utils.s.sol';
+import {ArbitrumScript,EthereumScript} from 'aave-helpers/../script/Utils.s.sol';
 import {AaveV3ArbBorrowCapsUpdate_20230410} from './AaveV3ArbBorrowCapsUpdate_20230410.sol';
 
-contract CreateProposal is ArbitrumScript {
+contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
-    payloads[0] = GovHelpers.buildArbitrum(address(0));
-    GovHelpers.createProposal(payloads, '');
+    payloads[0] = GovHelpers.buildArbitrum(0x31976DC2Ea27E75cC5a3687ED594D17127f9b72E);
+    GovHelpers.createProposal(payloads, 0x464496497d766fa1724c5fb3e6f501d7c89ed662109536184efdf6d925789186);
   }
 }
 
