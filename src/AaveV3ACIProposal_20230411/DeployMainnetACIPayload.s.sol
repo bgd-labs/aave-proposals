a66ddc1c435a83cd688533a729b1674b8980d8f2
Lukas <lukasstrassel@googlemail.com>
2023-04-13 12:07:42 +0200

    fix: follow naming convention (#129)

--- src/AaveV3ACIProposal_20230411/DeployMainnetACIPayload.s.sol
+++ src/AaveV3ACIProposal_20230411/DeployMainnetACIPayload.s.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.16;
 import {EthereumScript} from 'aave-helpers/../script/Utils.s.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
-import {ProposalPayload} from 'src/AaveV3ACIProposal_20230411/AaveV3ACIProposal_20230411.sol';
+import {AaveV3ACIProposal_20230411} from 'src/AaveV3ACIProposal_20230411/AaveV3ACIProposal_20230411.sol';
 
 contract DeployMainnetACIPayload is EthereumScript {
   function run() external broadcast {
-    new ProposalPayload();
+    new AaveV3ACIProposal_20230411();
   }
 }
 
