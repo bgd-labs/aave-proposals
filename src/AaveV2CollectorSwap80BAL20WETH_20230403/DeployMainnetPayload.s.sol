fe2675f408dd12e9c5354cd3457bdfb25947dcaa
efecarranza <fcarr004@fiu.edu>
2023-04-21 11:13:10 -0400

    update scripts location

--- src/AaveV2CollectorSwap80BAL20WETH_20230403/DeployMainnetPayload.s.sol
+++ src/AaveV2CollectorSwap80BAL20WETH_20230403/DeployMainnetPayload.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {EthereumScript} from 'aave-helpers/../script/Utils.s.sol';
+import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
 import {MockSwap} from './MockSwap.sol';
 
 contract ExampleMainnetPayload is EthereumScript {
