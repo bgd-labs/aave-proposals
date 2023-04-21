d48aff340209f84d86c3bcee659b7cc3630772b8
Jonathan Reem <jonathan.reem@gmail.com>
2023-04-21 12:33:32 -0700

    Remove arbitrum change and update IPFS hash.

--- src/AaveV3PolCapsUpdate_20230421/DeployAaveV3PolCapsUpdate_20230421.s.sol
+++ src/AaveV3PolCapsUpdate_20230421/DeployAaveV3PolCapsUpdate_20230421.s.sol
@@ -6,11 +6,9 @@ import {AaveV3PolCapsUpdate_20230421} from './AaveV3PolCapsUpdate_20230421.sol';
 
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
-    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](2);
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
     payloads[0] = GovHelpers.buildPolygon(0x33DeAc0861FD6a9235b86172AA939E79085c6f52);
-    // Deployed in an earlier proposal as part of the 03/27/2023 caps update
-    payloads[1] = GovHelpers.buildArbitrum(0x4c68fDA91bfb4683eAB90017d9B76a99F2d77Eed);
-    GovHelpers.createProposal(payloads, 0x3fbea0de2bd356382509565e0500cc73b102a4761976bcc409bd8636faa90b0a);
+    GovHelpers.createProposal(payloads, 0x8e036cb7b5c10028564ba8d16c19e1b8bda48fa41edb904caec571908a76b1d3);
   }
 }
 
