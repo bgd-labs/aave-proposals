fec15493e1aa581ebb6522cfe6eda9b1283060c9
Lukas <lukasstrassel@googlemail.com>
2023-04-27 17:22:08 +0200

    fix: update aci proposal

--- src/AaveV3RiskSteward_20230404/AaveV3RiskSteward_20230404_Test.t.sol
+++ src/AaveV3RiskSteward_20230404/AaveV3RiskSteward_20230404_Test.t.sol
@@ -8,17 +8,19 @@ import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 import {ProtocolV3_0_1TestBase, ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3RiskSteward_20230404} from './AaveV3RiskSteward_20230404.sol';
-import {CapsPlusRiskSteward, RiskStewardLibrary} from './CapsPlusRiskSteward.sol';
+import {CapsPlusRiskSteward} from 'aave-helpers/riskstewards/CapsPlusRiskSteward.sol';
+import {ICapsPlusRiskSteward} from 'aave-helpers/riskstewards/ICapsPlusRiskSteward.sol';
+import {IAaveV3ConfigEngine} from 'aave-helpers/v3-config-engine/IAaveV3ConfigEngine.sol';
 
 contract AaveV3RiskSteward_20230321_Test is TestWithExecutor, ProtocolV3_0_1TestBase {
   address public constant user = address(42);
-  CapsPlusRiskSteward public steward;
+  ICapsPlusRiskSteward public steward;
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 17020741);
     steward = new CapsPlusRiskSteward(
-      AaveV3Ethereum.POOL_CONFIGURATOR,
       AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER,
+      IAaveV3ConfigEngine(AaveV3Ethereum.LISTING_ENGINE),
       user
     );
     AaveV3RiskSteward_20230404 proposalPayload = new AaveV3RiskSteward_20230404(
@@ -35,8 +37,8 @@ contract AaveV3RiskSteward_20230321_Test is TestWithExecutor, ProtocolV3_0_1Test
       .AAVE_PROTOCOL_DATA_PROVIDER
       .getReserveCaps(AaveV3EthereumAssets.DAI_UNDERLYING);
 
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
+    IAaveV3ConfigEngine.CapsUpdate[] memory capUpdates = new IAaveV3ConfigEngine.CapsUpdate[](1);
+    capUpdates[0] = IAaveV3ConfigEngine.CapsUpdate(
       AaveV3EthereumAssets.DAI_UNDERLYING,
       daiSupplyCapBefore * 2,
       daiBorrowCapBefore * 2
@@ -49,177 +51,12 @@ contract AaveV3RiskSteward_20230321_Test is TestWithExecutor, ProtocolV3_0_1Test
       .AAVE_PROTOCOL_DATA_PROVIDER
       .getReserveCaps(AaveV3EthereumAssets.DAI_UNDERLYING);
 
-    (uint40 supplyCapLastUpdated, uint40 borrowCapLastUpdated) = steward.timelocks(
+    ICapsPlusRiskSteward.Debounce memory debounce = steward.getTimelock(
       AaveV3EthereumAssets.DAI_UNDERLYING
     );
     assertEq(daiBorrowCapAfter, capUpdates[0].borrowCap);
     assertEq(daiSupplyCapAfter, capUpdates[0].supplyCap);
-    assertEq(supplyCapLastUpdated, block.timestamp);
-    assertEq(borrowCapLastUpdated, block.timestamp);
-  }
-
-  function test_keepCurrent() public {
-    (uint256 daiBorrowCapBefore, uint256 daiSupplyCapBefore) = AaveV3Ethereum
-      .AAVE_PROTOCOL_DATA_PROVIDER
-      .getReserveCaps(AaveV3EthereumAssets.DAI_UNDERLYING);
-
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
-      AaveV3EthereumAssets.DAI_UNDERLYING,
-      RiskStewardLibrary.KEEP_CURRENT,
-      RiskStewardLibrary.KEEP_CURRENT
-    );
-
-    vm.startPrank(user);
-    steward.updateCaps(capUpdates);
-
-    (uint256 daiBorrowCapAfter, uint256 daiSupplyCapAfter) = AaveV3Ethereum
-      .AAVE_PROTOCOL_DATA_PROVIDER
-      .getReserveCaps(AaveV3EthereumAssets.DAI_UNDERLYING);
-
-    (uint40 supplyCapLastUpdated, uint40 borrowCapLastUpdated) = steward.timelocks(
-      AaveV3EthereumAssets.DAI_UNDERLYING
-    );
-    assertEq(daiBorrowCapAfter, daiBorrowCapBefore);
-    assertEq(daiSupplyCapAfter, daiSupplyCapBefore);
-    assertEq(supplyCapLastUpdated, 0);
-    assertEq(borrowCapLastUpdated, 0);
-  }
-
-  function test_invalidCaller() public {
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
-      AaveV3EthereumAssets.DAI_UNDERLYING,
-      RiskStewardLibrary.KEEP_CURRENT,
-      RiskStewardLibrary.KEEP_CURRENT
-    );
-
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.INVALID_CALLER)));
-    }
-  }
-
-  function test_updateSupplyCapBiggerMax() public {
-    (, uint256 daiSupplyCapBefore) = AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER.getReserveCaps(
-      AaveV3EthereumAssets.DAI_UNDERLYING
-    );
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
-      AaveV3EthereumAssets.DAI_UNDERLYING,
-      (daiSupplyCapBefore * 2) + 1,
-      RiskStewardLibrary.KEEP_CURRENT
-    );
-
-    vm.startPrank(user);
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.UPDATE_BIGGER_MAX)));
-    }
-  }
-
-  function test_updateBorrowCapBiggerMax() public {
-    (uint256 daiBorrowCapBefore, ) = AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER.getReserveCaps(
-      AaveV3EthereumAssets.DAI_UNDERLYING
-    );
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
-      AaveV3EthereumAssets.DAI_UNDERLYING,
-      RiskStewardLibrary.KEEP_CURRENT,
-      (daiBorrowCapBefore * 2) + 1
-    );
-
-    vm.startPrank(user);
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.UPDATE_BIGGER_MAX)));
-    }
-  }
-
-  function test_updateSupplyCapNotStrictlyHigher() public {
-    (, uint256 daiSupplyCapBefore) = AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER.getReserveCaps(
-      AaveV3EthereumAssets.DAI_UNDERLYING
-    );
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
-      AaveV3EthereumAssets.DAI_UNDERLYING,
-      daiSupplyCapBefore,
-      RiskStewardLibrary.KEEP_CURRENT
-    );
-
-    vm.startPrank(user);
-    // should fail when cap is equal current value
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.NOT_STICTLY_HIGHER)));
-    }
-
-    // should also fail when lower
-    capUpdates[0].supplyCap -= 1;
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.NOT_STICTLY_HIGHER)));
-    }
-  }
-
-  function test_updateBorrowCapNotStrictlyHigher() public {
-    (uint256 daiBorrowCapBefore, ) = AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER.getReserveCaps(
-      AaveV3EthereumAssets.DAI_UNDERLYING
-    );
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
-      AaveV3EthereumAssets.DAI_UNDERLYING,
-      RiskStewardLibrary.KEEP_CURRENT,
-      daiBorrowCapBefore
-    );
-
-    vm.startPrank(user);
-    // should fail when cap is equal current value
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.NOT_STICTLY_HIGHER)));
-    }
-
-    // should also fail when lower
-    capUpdates[0].borrowCap -= 1;
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.NOT_STICTLY_HIGHER)));
-    }
-  }
-
-  function test_debounce() public {
-    (uint256 daiBorrowCapBefore, ) = AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER.getReserveCaps(
-      AaveV3EthereumAssets.DAI_UNDERLYING
-    );
-
-    RiskStewardLibrary.CapUpdate[] memory capUpdates = new RiskStewardLibrary.CapUpdate[](1);
-    capUpdates[0] = RiskStewardLibrary.CapUpdate(
-      AaveV3EthereumAssets.DAI_UNDERLYING,
-      RiskStewardLibrary.KEEP_CURRENT,
-      daiBorrowCapBefore + 1
-    );
-
-    vm.startPrank(user);
-    steward.updateCaps(capUpdates);
-
-    capUpdates[0].borrowCap += 1;
-    try steward.updateCaps(capUpdates) {
-      require(false, 'MUST_FAIL');
-    } catch Error(string memory reason) {
-      require(
-        keccak256(bytes(reason)) == keccak256(bytes(RiskStewardLibrary.DECOUNCE_NOT_RESPECTED))
-      );
-    }
-
-    vm.warp(block.timestamp + steward.MINIMUM_DELAY() + 1);
-    steward.updateCaps(capUpdates);
+    assertEq(debounce.supplyCapLastUpdated, block.timestamp);
+    assertEq(debounce.borrowCapLastUpdated, block.timestamp);
   }
 }
