4c0b1ed237449baae469c86fed789dfa01ecae73
altmark <michala@chaoslabs.xyz>
2023-04-27 16:04:41 +0300

    update contract

--- src/AaveV3PolRiskParams_20230423/AaveV3PolRiskParams_20230423.sol
+++ src/AaveV3PolRiskParams_20230423/AaveV3PolRiskParams_20230423.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.16;
 import 'aave-helpers/v3-config-engine/AaveV3PayloadPolygon.sol';
 
 /**
- * @title Update eurs borrow cap on Aave V3 Polygon
- * @author Gauntlet
- * - Snapshot: N/A
- * - Discussion: https://governance.aave.com/t/gauntlet-recommendations-for-eurs-on-v3-polygon/12798
+ * @title Update Risk Parameters for WBTC, DAI, LINK and WMATIC Polygon
+ * @author @ChaosLabsInc
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x6dfc0cf7c95e91cfa9643a2621617c8210079aa2d44bdf8574ff1425a9ae52f0
+ * - Discussion: https://governance.aave.com/t/arfc-chaos-labs-risk-parameter-updates-aave-v3-polygon-2023-04-23/12828
  */
 contract AaveV3PolRiskParams_20230423 is AaveV3PayloadPolygon {
   function collateralsUpdates() public pure override returns (IEngine.CollateralUpdate[] memory) {
