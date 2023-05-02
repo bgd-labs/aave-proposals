120b1a5a9586b7b2c5800262f397bd90eb6ca357
altmark <michala@chaoslabs.xyz>
2023-05-02 14:38:38 +0300

    update hash and contract

--- src/AaveV3PolRiskParams_20230423/DeployAaveV3PolRiskParams_20230423.s.sol
+++ src/AaveV3PolRiskParams_20230423/DeployAaveV3PolRiskParams_20230423.s.sol
@@ -7,8 +7,8 @@ import {AaveV3PolRiskParams_20230423} from './AaveV3PolRiskParams_20230423.sol';
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
-    payloads[0] = GovHelpers.buildPolygon(address(0)); // TODO: contract address
-    GovHelpers.createProposal(payloads, 0); // TODO: AIP hash
+    payloads[0] = GovHelpers.buildPolygon(address(0xc53586aa2626094bd33c123794e34417ea877a36));
+    GovHelpers.createProposal(payloads, 0x0534109ab793da6816093b041e5f1685ed1f2523ad69c7e734a85a5bcf22895b); 
   }
 }
 
