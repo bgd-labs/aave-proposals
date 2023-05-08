1f92efd0a85b21035f3bb3c6d527ee90f3b5006f
efecarranza <fcarr004@fiu.edu>
2023-05-08 19:24:02 -0400

    write payload

--- src/AaveV3sdCRV_20230503/AaveV3EthsdCRV_20230503_Payload.sol
+++ src/AaveV3sdCRV_20230503/AaveV3EthsdCRV_20230503_Payload.sol
@@ -1,6 +1,57 @@
-// SPDX-License-Identifier
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract AaveV3EthsdCRV_20230503_Payload {
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
 
+interface ICRVDepositor {
+    /// @notice Deposit & Lock Token
+	/// @dev User needs to approve the contract to transfer the token
+	/// @param _amount The amount of token to deposit
+	/// @param _lock Whether to lock the token
+	/// @param _stake Whether to stake the token
+	/// @param _user User to deposit for
+	function deposit(
+		uint256 _amount,
+		bool _lock,
+		bool _stake,
+		address  _user
+	) external;
+}
+
+contract AaveV3EthsdCRV_20230503_Payload is IProposalGenericExecutor {
+    address public constant CRV_DEPOSITOR = 0xc1e3Ca8A3921719bE0aE3690A0e036feB4f69191;
+
+    function execute() external {
+        /*******************************************************************************
+        ******************************** Withdraw Tokens *******************************
+        *******************************************************************************/
+        AaveV2Ethereum.COLLECTOR.transfer(
+            AaveV2EthereumAssets.CRV_A_TOKEN,
+            address(this),
+            IERC20(AaveV2EthereumAssets.CRV_A_TOKEN).balanceOf(address(AaveV2Ethereum.COLLECTOR))
+        );
+
+        AaveV2Ethereum.COLLECTOR.transfer(
+            AaveV2EthereumAssets.CRV_UNDERLYING,
+            address(this),
+            IERC20(AaveV2EthereumAssets.CRV_UNDERLYING).balanceOf(address(AaveV2Ethereum.COLLECTOR))
+        );
+
+        AaveV2Ethereum.POOL.withdraw(
+            AaveV2EthereumAssets.CRV_UNDERLYING,
+            type(uint256).max,
+            address(this)
+        );
+
+        /*******************************************************************************
+        *********************************** Deposit ***********************************
+        *******************************************************************************/
+        uint256 balance = IERC20(AaveV2EthereumAssets.CRV_UNDERLYING).balanceOf(address(this));
+        
+        IERC20(AaveV2EthereumAssets.CRV_UNDERLYING).approve(CRV_DEPOSITOR, balance);
+
+        ICRVDepositor(CRV_DEPOSITOR).deposit(balance, true, false, address(AaveV2Ethereum.COLLECTOR));
+    }
 }
---
+++ src/AaveV3sdCRV_20230503/AaveV3EthsdCRV_20230503_PayloadTest.t.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
+
+import {AaveV3EthsdCRV_20230503_Payload} from './AaveV3EthsdCRV_20230503_Payload.sol';
+
+contract AaveV3PolCapsUpdates_20230328Test is ProtocolV3TestBase, TestWithExecutor {
+    address public constant SD_CRV = 0xD1b5651E55D4CeeD36251c61c50C889B36F6abB5;
+  AaveV3EthsdCRV_20230503_Payload payload;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 17219011);
+    _selectPayloadExecutor(AaveGovernanceV2.SHORT_EXECUTOR);
+
+    payload = new AaveV3EthsdCRV_20230503_Payload();
+  }
+
+  function testExecute() public {
+    // Pre-execution balances of CRV and aCRV
+    assertEq(
+      IERC20(AaveV2Ethereum.CRV_UNDERLYING).balanceOf(
+        address(AaveV2Ethereum.COLLECTOR),
+        0
+      )
+    );
+    assertEq(
+      IERC20(AaveV2Ethereum.CRV_A_TOKEN).balanceOf(
+        address(AaveV2Ethereum.COLLECTOR),
+        0
+      )
+    );
+    // End Pre-execution balances of CRV and aCRV
+
+    _executePayload(address(payload));
+
+    // Post-execution balances of CRV and aCRV
+    assertEq(
+      IERC20(AaveV2Ethereum.CRV_UNDERLYING).balanceOf(
+        address(AaveV2Ethereum.COLLECTOR),
+        0
+      )
+    );
+    assertEq(
+      IERC20(AaveV2Ethereum.CRV_A_TOKEN).balanceOf(
+        address(AaveV2Ethereum.COLLECTOR),
+        0
+      )
+    );
+
+    assertEq(
+      IERC20(SD_CRV).balanceOf(
+        address(AaveV2Ethereum.COLLECTOR),
+        0
+      )
+    );
+
+    vm.startPrank(payload.AGD_MULTISIG());
+    IERC20(AaveV2EthereumAssets.USDT_A_TOKEN).transferFrom(
+      address(AaveV2Ethereum.COLLECTOR),
+      makeAddr('new-addy'),
+      payload.AMOUNT_AUSDT()
+    );
+    vm.stopPrank();
+    // End Post-execution balances of CRV and aCRV
+}
+}
