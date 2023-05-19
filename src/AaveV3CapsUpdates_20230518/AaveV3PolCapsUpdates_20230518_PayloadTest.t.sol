6f2be728371448fb082a2d6e64a0b1e2cf7839c5
Fermin 'Piscu' Carranza <fcarr004@fiu.edu>
2023-05-19 04:26:08 -0400

    Aave V3 Polygon: Update wMATIC Caps (#154)
    * payload
    
    * update to 50m

---
+++ src/AaveV3CapsUpdates_20230518/AaveV3PolCapsUpdates_20230518_PayloadTest.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {AaveV3Polygon, AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
+import {AaveV3PolCapsUpdates_20230518_Payload} from './AaveV3PolCapsUpdates_20230518_Payload.sol';
+
+contract AaveV3PolCapsUpdates_20230518_PayloadTest is ProtocolV3TestBase, TestWithExecutor {
+  AaveV3PolCapsUpdates_20230518_Payload payload;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('polygon'), 42863668);
+    _selectPayloadExecutor(AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR);
+
+    payload = new AaveV3PolCapsUpdates_20230518_Payload();
+  }
+
+  function testCapsUpdates() public {
+    ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot(
+      'pre-Aave-V3-Polygon-Caps-Updates-20230518',
+      AaveV3Polygon.POOL
+    );
+
+    _executePayload(address(payload));
+
+    ReserveConfig memory wMATIC = _findReserveConfig(
+      allConfigsBefore,
+      AaveV3PolygonAssets.WMATIC_UNDERLYING
+    );
+
+    wMATIC.supplyCap = payload.NEW_SUPPLY_CAP();
+    wMATIC.borrowCap = payload.NEW_BORROW_CAP();
+
+    ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot(
+      'post-Aave-V3-Polygon-Caps-Updates-20230518',
+      AaveV3Polygon.POOL
+    );
+
+    address[] memory assetsChanged = new address[](1);
+    assetsChanged[0] = AaveV3PolygonAssets.WMATIC_UNDERLYING;
+
+    _noReservesConfigsChangesApartFrom(allConfigsBefore, allConfigsAfter, assetsChanged);
+
+    _validateReserveConfig(wMATIC, allConfigsAfter);
+
+    diffReports(
+      'pre-Aave-V3-Polygon-Caps-Updates-20230518',
+      'post-Aave-V3-Polygon-Caps-Updates-20230518'
+    );
+  }
+}
