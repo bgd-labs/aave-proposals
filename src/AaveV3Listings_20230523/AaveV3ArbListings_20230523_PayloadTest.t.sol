6a13021f4fbdaa4c8b4e7f239d022ff08e7ae9f5
efecarranza <fcarr004@fiu.edu>
2023-05-24 08:58:05 -0400

    separate reth

--- src/AaveV3Listings_20230523/AaveV3ArbListings_20230523_PayloadTest.t.sol
+++ src/AaveV3Listings_20230523/AaveV3ArbListings_20230523_PayloadTest.t.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.19;
 
-import 'forge-std/Test.sol';
 import {AaveV3Arbitrum, AaveV3ArbitrumAssets} from 'aave-address-book/AaveV3Arbitrum.sol';
 import {ProtocolV3TestBase, InterestStrategyValues, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
@@ -27,58 +26,7 @@ contract AaveV3ArbListings_20230523_PayloadTest is ProtocolV3TestBase, TestWithE
 
     ReserveConfig[] memory allConfigs = createConfigurationSnapshot('post-Aave-V3-Arbitrum-Listings-20230523', AaveV3Arbitrum.POOL);
 
-    ReserveConfig memory rETH = ReserveConfig({
-      symbol: 'rETH',
-      underlying: payload.RETH(),
-      aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-      variableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-      stableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-      decimals: 18,
-      ltv: 67_00,
-      liquidationThreshold: 74_00,
-      liquidationBonus: 10750,
-      liquidationProtocolFee: 1000,
-      reserveFactor: 1500,
-      usageAsCollateralEnabled: true,
-      borrowingEnabled: true,
-      interestRateStrategy: _findReserveConfig(allConfigs, payload.RETH()).interestRateStrategy,
-      stableBorrowRateEnabled: false,
-      isActive: true,
-      isFrozen: false,
-      isSiloed: false,
-      isBorrowableInIsolation: false,
-      isFlashloanable: false,
-      supplyCap: 325,
-      borrowCap: 85,
-      debtCeiling: 0,
-      eModeCategory: 0
-    });
-
-    _validateReserveConfig(rETH, allConfigs);
-
-    _validateInterestRateStrategy(
-      rETH.interestRateStrategy,
-      rETH.interestRateStrategy,
-      InterestStrategyValues({
-        addressesProvider: address(AaveV3Arbitrum.POOL_ADDRESSES_PROVIDER),
-        optimalUsageRatio: 45 * (RAY / 100),
-        optimalStableToTotalDebtRatio: 20 * (RAY / 100),
-        baseStableBorrowRate: 10 * (RAY / 100),
-        stableRateSlope1: 13 * (RAY / 100),
-        stableRateSlope2: 300 * (RAY / 100),
-        baseVariableBorrowRate: 0,
-        variableRateSlope1: 7 * (RAY / 100),
-        variableRateSlope2: 300 * (RAY / 100)
-      })
-    );
-
-    _validateAssetSourceOnOracle(
-      AaveV3Arbitrum.POOL_ADDRESSES_PROVIDER,
-      payload.RETH(),
-      payload.RETH_PRICE_FEED()
-    );
-
-     ReserveConfig memory lusd = ReserveConfig({
+    ReserveConfig memory lusd = ReserveConfig({
       symbol: 'LUSD',
       underlying: payload.LUSD(),
       aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
