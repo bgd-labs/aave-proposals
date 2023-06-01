63d4061e06c720ed9919d89d5c79d4f983198328
Lukas <lukasstrassel@googlemail.com>
2023-06-01 23:20:07 +0200

    fix: finally fix ci

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-readarray -t json_array < <(jq .[] "addedFiles.json")
-declare -p json_array
-for i in "${json_array[@]}"
+json_array=($(jq -r '.[]' "addedFiles.json"))
+for i in ${!json_array[@]}
 do
-  npx aave-cli ipfs "$i" -u true
+  npx aave-cli ipfs $(printf '%s\n' ${json_array[$i]}) -u true
 done
