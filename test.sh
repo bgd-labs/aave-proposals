808d3f5fe127fa8fcbf18e653abfaac6c8e04829
Lukas <lukasstrassel@googlemail.com>
2023-06-01 17:56:39 +0200

    test: try ci

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-json='["src/Test/test.md","src/Test/test2.md"]'
-readarray -t json_array < <(jq -r '.[]' <<<"$json")
+readarray -t json_array < <(jq .[] "addedFiles.json")
 declare -p json_array
 for i in "${json_array[@]}"
 do
-   npx aave-cli ipfs "$i" -u true
+  npx aave-cli ipfs "$i" -u true
 done
