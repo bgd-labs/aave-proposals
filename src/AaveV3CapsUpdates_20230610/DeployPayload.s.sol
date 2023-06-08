1565728dd803e6085b91c6dda3a21a33296b28b5
efecarranza <fcarr004@fiu.edu>
2023-06-08 16:49:23 -0400

    add supply cap PR

---
+++ src/AaveV3CapsUpdates_20230610/DeployPayload.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {EthereumScript, PolygonScript} from 'aave-helpers/ScriptUtils.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveV3PolCapsUpdates_20230610_Payload} from 'src/AaveV3CapsUpdates_20230610/AaveV3PolCapsUpdates_20230610_Payload.sol';
+
+contract DeployMainnetPayload is PolygonScript {
+  function run() external broadcast {
+    new AaveV3PolCapsUpdates_20230610_Payload();
+  }
+}
+
+contract PayloadProposal is EthereumScript {
+  function run() external broadcast {
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
+    payloads[0] = GovHelpers.buildMainnet(
+      address(0) // TODO: Replace by actual payload
+    );
+    GovHelpers.createProposal(
+      payloads,
+      GovHelpers.ipfsHashFile(
+        vm,
+        'src/AaveV3CapsUpdates_20230610/AAVE-V3-POL-CAPS-UPDATES-20230610.md',
+        // if you set `upload` to `true`, your env must contain PINATA_KEY & PINATA_SECRET
+        // the file will be uploaded automatically once merged to main
+        false
+      )
+    );
+  }
+}
