91ad7fefe6edb34d710f9abbd40af0116b04c8f0
defijesus.eth <defijesus@buzzedbearhideout.com>
2023-06-19 19:11:33 +0100

    add aave v3 eth frax listing

---
+++ src/AaveV3EthFraxListing_20230619/DeployAaveV3EthFraxListing.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveV3EthFraxListing_20230619} from 'src/AaveV3EthFraxListing_20230619/AaveV3EthFraxListing_20230619.sol';
+
+contract DeployAaveV3FraxListingPayload is EthereumScript {
+  function run() external broadcast {
+    new AaveV3EthFraxListing_20230619();
+  }
+}
+
+contract AaveV3FraxListingPayloadProposal is EthereumScript {
+  function run() external broadcast {
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
+    payloads[0] = GovHelpers.buildPolygon(
+      address(0) //TODO
+    );
+    GovHelpers.createProposal(
+      payloads,
+      GovHelpers.ipfsHashFile(
+        vm,
+        'src/AaveV3EthFraxListing_20230619/TODO.md'
+      )
+    );
+  }
+}
