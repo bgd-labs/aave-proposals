f64e2ea497547823e4bc14cddd06ff089d9502df
marczeller <pierarte@gmail.com>
2023-06-21 13:29:58 +0200

    init

---
+++ src/AaveV3ArbNativeUSDCListing_20230621/DeployUSDCListing.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+import {EthereumScript, ArbitrumScript} from 'aave-helpers/ScriptUtils.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveV3ArbNativeUSDCListing_20230621} from 'src/AaveV3ArbNativeUSDCListing_20230621/AaveV3ArbNativeUSDCListing_20230621.sol';
+
+contract DeployArbUSDNPayload is ArbitrumScript {
+  function run() external broadcast {
+    new AaveV3ArbNativeUSDCListing_20230621();
+  }
+}
+
+contract USDCNPayloadProposal is EthereumScript {
+  function run() external broadcast {
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
+    payloads[0] = GovHelpers.buildArbitrum(
+      address(0) // TODO: Replace by actual payload
+    );
+    GovHelpers.createProposal(
+      payloads,
+      GovHelpers.ipfsHashFile(
+        vm,
+        'src/AaveV3ArbNativeUSDCListing_20230621/ADD-NATIVE-USDC-ARB-V3-AIP.md',
+        // if you set `upload` to `true`, your env must contain PINATA_KEY & PINATA_SECRET
+        // the file will be uploaded automatically once merged to main
+        false
+      )
+    );
+  }
+}
