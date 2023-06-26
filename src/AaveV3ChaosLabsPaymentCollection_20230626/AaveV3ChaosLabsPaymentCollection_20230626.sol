e1dfa6434023a30ce0bab1989ab4448a6eb752a8
yonikesel <yonikesel@chaoslabs.xyz>
2023-06-26 14:26:12 +0300

    formatting

--- src/AaveV3ChaosLabsPaymentCollection_20230626/AaveV3ChaosLabsPaymentCollection_20230626.sol
+++ src/AaveV3ChaosLabsPaymentCollection_20230626/AaveV3ChaosLabsPaymentCollection_20230626.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import {IProposalGenericExecutor} from "aave-helpers/interfaces/IProposalGenericExecutor.sol";
-import {AaveMisc, IAaveEcosystemReserveController} from "aave-address-book/AaveMisc.sol";
-import {AaveV2Ethereum, AaveV2EthereumAssets} from "aave-address-book/AaveV2Ethereum.sol";
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+import {AaveMisc, IAaveEcosystemReserveController} from 'aave-address-book/AaveMisc.sol';
+import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
 
 /**
  * @title ChaosLabs Service Provider Payment Collection
@@ -12,13 +12,16 @@ import {AaveV2Ethereum, AaveV2EthereumAssets} from "aave-address-book/AaveV2Ethe
  * - Discussion: https://governance.aave.com/t/arfc-chaos-labs-payment-collection-request/13792
  */
 contract AaveV3ChaosLabsPaymentCollection_20230626 is IProposalGenericExecutor {
-    address public constant CHAOS_LABS = 0xbC540e0729B732fb14afA240aA5A047aE9ba7dF0;
-    address public constant AAVE = AaveV2EthereumAssets.AAVE_UNDERLYING;
-    uint256 public constant PAYMENT_AMOUNT = 6541e18;
+  address public constant CHAOS_LABS = 0xbC540e0729B732fb14afA240aA5A047aE9ba7dF0;
+  address public constant AAVE = AaveV2EthereumAssets.AAVE_UNDERLYING;
+  uint256 public constant PAYMENT_AMOUNT = 6541e18;
 
-    function execute() external {
-        AaveMisc.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.transfer(
-            AaveMisc.ECOSYSTEM_RESERVE, AaveV2EthereumAssets.AAVE_UNDERLYING, CHAOS_LABS, PAYMENT_AMOUNT
-        );
-    }
+  function execute() external {
+    AaveMisc.AAVE_ECOSYSTEM_RESERVE_CONTROLLER.transfer(
+      AaveMisc.ECOSYSTEM_RESERVE,
+      AaveV2EthereumAssets.AAVE_UNDERLYING,
+      CHAOS_LABS,
+      PAYMENT_AMOUNT
+    );
+  }
 }
