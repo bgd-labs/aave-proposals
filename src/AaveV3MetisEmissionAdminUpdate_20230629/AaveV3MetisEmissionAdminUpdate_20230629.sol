3d25d09427f4ae552fb120846f9ce57ca5075d69
Rami Husami <husamirami@gmail.com>
2023-06-29 13:15:41 +0400

    remove constructor

--- src/AaveV3MetisEmissionAdminUpdate_20230629/AaveV3MetisEmissionAdminUpdate_20230629.sol
+++ src/AaveV3MetisEmissionAdminUpdate_20230629/AaveV3MetisEmissionAdminUpdate_20230629.sol
@@ -12,17 +12,10 @@ import {IEmissionManager} from 'aave-v3-periphery/rewards/interfaces/IEmissionMa
  * Once executed this payload would add an EMISSION_ADMIN for METIS token on the Metis EMISSION_MANAGER.
  */
 contract AaveV3MetisEmissionAdminUpdate_20230629 is IProposalGenericExecutor {
-  IEmissionManager public immutable EMISSION_MANAGER;
+  IEmissionManager public constant EMISSION_MANAGER = IEmissionManager(AaveV3Metis.EMISSION_MANAGER);
 
-  address public immutable METIS;
-
-  address public immutable EMISSION_ADMIN;
-
-  constructor() {
-    EMISSION_MANAGER = IEmissionManager(AaveV3Metis.EMISSION_MANAGER);
-    METIS = AaveV3MetisAssets.Metis_UNDERLYING;
-    EMISSION_ADMIN = 0x97177cD80475f8b38945c1E77e12F0c9d50Ac84D;
-  }
+  address public constant METIS = AaveV3MetisAssets.Metis_UNDERLYING;
+  address public constant EMISSION_ADMIN = 0x97177cD80475f8b38945c1E77e12F0c9d50Ac84D;
 
   function execute() public {
     EMISSION_MANAGER.setEmissionAdmin(METIS, EMISSION_ADMIN);
