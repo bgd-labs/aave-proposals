a8daede53085589f0b7e85fb76ce81a53e61ae99
Rustem Kurmaev <rustboyar@gmail.com>
2023-06-29 14:23:54 +0300

    Use general e2e test (#192)

--- src/AaveV3PriceFeedsUpdate_20230626/AaveV3PolPriceFeedsUpdate_20230626_PayloadTest.t.sol
+++ src/AaveV3PriceFeedsUpdate_20230626/AaveV3PolPriceFeedsUpdate_20230626_PayloadTest.t.sol
@@ -5,13 +5,12 @@ import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
 import {AaveV3Polygon, AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
-import {ProtocolV3TestBase} from 'aave-helpers/ProtocolV3TestBase.sol';
-import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3PolPriceFeedsUpdate_20230626_Payload} from './AaveV3PolPriceFeedsUpdate_20230626_Payload.sol';
 
 contract AaveV3PolPriceFeedsUpdate_20230626_PayloadTest is ProtocolV3TestBase, TestWithExecutor {
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('polygon'),44448455);
+    vm.createSelectFork(vm.rpcUrl('polygon'), 44448455);
     _selectPayloadExecutor(AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR);
   }
 
@@ -28,7 +27,7 @@ contract AaveV3PolPriceFeedsUpdate_20230626_PayloadTest is ProtocolV3TestBase, T
     _executePayload(address(payload));
 
     // 3. create snapshot after payload execution
-    createConfigurationSnapshot(
+    ReserveConfig[] memory configs = createConfigurationSnapshot(
       'postAaveV3PolPriceFeedsUpdate_20230626_PayloadChange',
       AaveV3Polygon.POOL
     );
@@ -47,20 +46,10 @@ contract AaveV3PolPriceFeedsUpdate_20230626_PayloadTest is ProtocolV3TestBase, T
     );
 
     // 6. e2e
-    address user = address(9999);
-    uint256 amount = 3000 * 10e6;
-
-    deal(AaveV3PolygonAssets.USDC_UNDERLYING, user, amount);
-
-    vm.startPrank(user);
-
-    IERC20(AaveV3PolygonAssets.USDC_UNDERLYING).approve(address(AaveV3Polygon.POOL), amount);
-    AaveV3Polygon.POOL.deposit(AaveV3PolygonAssets.USDC_UNDERLYING, amount, user, 0);
-    AaveV3Polygon.POOL.borrow(AaveV3PolygonAssets.wstETH_UNDERLYING, 1 * 10e18, 2, 0, user);
-
-    vm.expectRevert();
-    AaveV3Polygon.POOL.borrow(AaveV3PolygonAssets.wstETH_UNDERLYING, 1 * 10e18, 2, 0, user);
-
-    vm.stopPrank();
+    e2eTestAsset(
+      AaveV3Polygon.POOL,
+      _findReserveConfig(configs, AaveV3PolygonAssets.DAI_UNDERLYING),
+      _findReserveConfig(configs, AaveV3PolygonAssets.wstETH_UNDERLYING)
+    );
   }
 }
