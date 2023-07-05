57a7ee7744d1ae2b486a9e3c34376a5e69e51171
efecarranza <fcarr004@fiu.edu>
2023-07-05 07:46:40 -0400

    chore: extend forge base Test

--- src/AaveV2CollectorSwap80BAL20WETH_20230403/AaveV2CollectorSwapTest.t.sol
+++ src/AaveV2CollectorSwap80BAL20WETH_20230403/AaveV2CollectorSwapTest.t.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {console2} from 'forge-std/Test.sol';
+import {Test} from 'forge-std/Test.sol';
 
-import {ProtocolV3LegacyTestBase} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
@@ -13,7 +12,7 @@ import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
 import {SwapFor80BAL20WETHPayload} from './AaveV2CollectorSwap80BAL20WETH_20230403_Payload.sol';
 import {COWTrader} from './COWTrader.sol';
 
-contract SwapFor80BAL20WETHPayloadTest is ProtocolV3LegacyTestBase {
+contract SwapFor80BAL20WETHPayloadTest is Test {
   event TradeCanceled();
   event TradeRequested();
 
