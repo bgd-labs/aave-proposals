0487b0690bcbaa82d2e491b5252693514861dd93
zer0dot <zer0dot.dev@gmail.com>
2023-07-10 15:41:14 -0400

    Create bug bounty payload

---
+++ src/AaveV3_Eth_BugBounty_20230710/AaveV3_Eth_BugBounty_20230710.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {ICollector} from 'aave-address-book/common/ICollector.sol';
+
+/**
+ * @title AaveV3_Eth_BugBounty_20230710
+ * @author BGD labs
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x6f70e60abd398e1ff04ff6a78cd313b69d47df84e42b790c14c273dc5ab31674
+ * - Discussion: https://governance.aave.com/t/bgd-bug-bounties-proposal/13077
+ */
+contract AaveV3_Eth_BugBounty_20230710 {
+  ICollector public constant COLLECTOR = AaveV3Ethereum.COLLECTOR;
+  address public constant FIRST_RECIPIENT = 0xb91F64b7CD46e5cF5C0735d42D8292576aD45FAb; // Kankodu
+  address public constant SECOND_RECIPIENT = 0x7dF98A6e1895fd247aD4e75B8EDa59889fa7310b; // Emanuele
+  address public constant THIRD_RECIPIENT = 0x7Ac71b1944869C13b36Bfb25D7623723d288e6B2; // cmichel
+  address public constant FOURTH_RECIPIENT = 0x192bDD30D272AabC2B1c3c719c518F0f2d10cc60; // Watchpug
+
+  uint256 public constant FIRST_AMOUNT = 20_000e6;
+  uint256 public constant SECOND_AMOUNT = 5000e6 + 10_000e6 + 20_000e6;
+  uint256 public constant THIRD_AMOUNT = 20_000e6;
+  uint256 public constant FOURTH_AMOUNT = 10_000e6;
+
+  function execute() external {
+    // Transfer bug bounties
+    COLLECTOR.transfer(AaveV2EthereumAssets.USDT_A_TOKEN, FIRST_RECIPIENT, FIRST_AMOUNT);
+    COLLECTOR.transfer(AaveV2EthereumAssets.USDT_A_TOKEN, SECOND_RECIPIENT, SECOND_AMOUNT);
+    COLLECTOR.transfer(AaveV2EthereumAssets.USDT_A_TOKEN, THIRD_RECIPIENT, THIRD_AMOUNT);
+    COLLECTOR.transfer(AaveV2EthereumAssets.USDT_A_TOKEN, FOURTH_RECIPIENT, FOURTH_AMOUNT);
+  }
+}
---
+++ src/AaveV3_Eth_BugBounty_20230710/AaveV3_Eth_BugBounty_20230710.t.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {AaveV3_Eth_BugBounty_20230710} from './AaveV3_Eth_BugBounty_20230710.sol';
+
+/**
+ * @dev Test for AaveV3_Eth_BugBounty_20230710
+ * command: make test-contract filter=AaveV3_Eth_BugBounty_20230710
+ */
+contract AaveV3_Eth_BugBounty_20230710_Test is ProtocolV3TestBase {
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 17664219);
+  }
+
+  function testProposalExecution() public {
+    AaveV3_Eth_BugBounty_20230710 proposal = new AaveV3_Eth_BugBounty_20230710();
+    IERC20 aUSDT = IERC20(AaveV2EthereumAssets.USDT_A_TOKEN);
+
+    uint256 totalAmountToDisburse = proposal.FIRST_AMOUNT() +
+      proposal.SECOND_AMOUNT() +
+      proposal.THIRD_AMOUNT() +
+      proposal.FOURTH_AMOUNT();
+
+    ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot(
+      'preAaveV3_Eth_BugBounty_20230710',
+      AaveV3Ethereum.POOL
+    );
+
+    uint256 amountCollectorBefore = aUSDT.balanceOf(address(AaveV3Ethereum.COLLECTOR));
+    uint256 amountRecipientOneBefore = aUSDT.balanceOf(proposal.FIRST_RECIPIENT());
+    uint256 amountRecipientTwoBefore = aUSDT.balanceOf(proposal.SECOND_RECIPIENT());
+    uint256 amountRecipientThreeBefore = aUSDT.balanceOf(proposal.THIRD_RECIPIENT());
+    uint256 amountRecipientFourBefore = aUSDT.balanceOf(proposal.FOURTH_RECIPIENT());
+
+    GovHelpers.executePayload(vm, address(proposal), AaveGovernanceV2.SHORT_EXECUTOR);
+
+    uint256 amountCollectorAfter = aUSDT.balanceOf(address(AaveV3Ethereum.COLLECTOR));
+    uint256 amountRecipientOneAfter = aUSDT.balanceOf(proposal.FIRST_RECIPIENT());
+    uint256 amountRecipientTwoAfter = aUSDT.balanceOf(proposal.SECOND_RECIPIENT());
+    uint256 amountRecipientThreeAfter = aUSDT.balanceOf(proposal.THIRD_RECIPIENT());
+    uint256 amountRecipientFourAfter = aUSDT.balanceOf(proposal.FOURTH_RECIPIENT());
+
+    assertAlmostEq(amountCollectorBefore, amountCollectorAfter + totalAmountToDisburse);
+    assertAlmostEq(amountRecipientOneBefore, amountRecipientOneAfter - proposal.FIRST_AMOUNT());
+    assertAlmostEq(amountRecipientTwoBefore, amountRecipientTwoAfter - proposal.SECOND_AMOUNT());
+    assertAlmostEq(amountRecipientThreeBefore, amountRecipientThreeAfter - proposal.THIRD_AMOUNT());
+    assertAlmostEq(amountRecipientFourBefore, amountRecipientFourAfter - proposal.FOURTH_AMOUNT());
+
+    ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot(
+      'postAaveV3_Eth_BugBounty_20230710',
+      AaveV3Ethereum.POOL
+    );
+
+    _noReservesConfigsChangesApartNewListings(allConfigsBefore, allConfigsAfter);
+  }
+
+  function assertAlmostEq(uint256 a, uint256 b) internal {
+    uint256 diff = a > b ? a - b : b - a;
+    assertLe(diff, 1);
+  }
+}
+
+interface IERC20 {
+  function balanceOf(address account) external view returns (uint256);
+}
