a43d74e84c0cbd7e08c7a35c66de81c77eac8bf1
Zer0dot <zer0dot.dev@gmail.com>
2023-07-11 08:37:45 -0400

    Update src/AaveV3_Eth_BugBounty_20230710/AaveV3_Eth_BugBounty_20230710.t.sol
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

--- src/AaveV3_Eth_BugBounty_20230710/AaveV3_Eth_BugBounty_20230710.t.sol
+++ src/AaveV3_Eth_BugBounty_20230710/AaveV3_Eth_BugBounty_20230710.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import 'forge-std/Test.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
-import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 import {AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3_Eth_BugBounty_20230710} from './AaveV3_Eth_BugBounty_20230710.sol';
