0c9009a16e8c32b8023bad21b04fbe928798e591
Marc Zeller <pierarte@gmail.com>
2023-07-11 14:55:39 +0200

    Update src/AaveV3_Eth_AaveV3ListRPL_20230711_20231107/AaveV3_Eth_AaveV3ListRPL_20230711_20231107.t.sol
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

--- src/AaveV3_Eth_AaveV3ListRPL_20230711_20231107/AaveV3_Eth_AaveV3ListRPL_20230711_20231107.t.sol
+++ src/AaveV3_Eth_AaveV3ListRPL_20230711_20231107/AaveV3_Eth_AaveV3ListRPL_20230711_20231107.t.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'forge-std/Test.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
-import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3_Eth_AaveV3ListRPL_20230711_20231107} from './AaveV3_Eth_AaveV3ListRPL_20230711_20231107.sol';
 
