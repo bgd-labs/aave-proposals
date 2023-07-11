7c1b5fffbd6b0d679110d08239b41f6b6fd83414
marczeller <pierarte@gmail.com>
2023-07-11 13:58:26 +0200

    rm unused import

--- src/AaveV3_Eth_AaveV3ListRPL_20230711_20231107/AaveV3_Eth_AaveV3ListRPL_20230711_20231107.sol
+++ src/AaveV3_Eth_AaveV3ListRPL_20230711_20231107/AaveV3_Eth_AaveV3ListRPL_20230711_20231107.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import {AaveV3PayloadEthereum, IEngine, Rates, EngineFlags} from 'aave-helpers/v3-config-engine/AaveV3PayloadEthereum.sol';
-import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 
 /**
  * @title Add RPL to Aave V3 pool
