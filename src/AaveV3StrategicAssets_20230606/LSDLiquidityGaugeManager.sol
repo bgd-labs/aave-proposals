b4b546131eafd2c2cde8d6465c883de716ab7e25
efecarranza <fcarr004@fiu.edu>
2023-07-12 09:58:33 -0400

    chore: remove unused functions

--- src/AaveV3StrategicAssets_20230606/LSDLiquidityGaugeManager.sol
+++ src/AaveV3StrategicAssets_20230606/LSDLiquidityGaugeManager.sol
@@ -48,49 +48,4 @@ abstract contract LSDLiquidityGaugeManager is Core {
     ILiquidityGaugeController(gaugeControllers[token]).vote_for_gauge_weights(gauge, weight);
     emit GaugeVote(gauge, weight);
   }
-
-  /// @notice Stake tokens in a gauge
-  /// @param token the address of the token to stake in the gauge
-  /// @param amount the amount of tokens to stake in the gauge
-  function stakeInGauge(address token, address gauge, uint256 amount) external onlyAdminOrManager {
-    if (gauge == address(0)) revert Invalid0xAddress();
-
-    IERC20(token).approve(gauge, amount);
-    ILiquidityGauge(gauge).deposit(amount);
-
-    emit GaugeStake(gauge, amount);
-  }
-
-  /// @notice Unstake tokens from a gauge
-  /// @param gauge the address of the gauge where token is staked
-  /// @param amount the amount of tokens to unstake from the gauge
-  function unstakeFromGauge(address gauge, uint256 amount) external onlyAdminOrManager {
-    if (gauge == address(0)) revert Invalid0xAddress();
-    ILiquidityGauge(gauge).withdraw(amount, false);
-
-    emit GaugeUnstake(gauge, amount);
-  }
-
-  /// @notice Claim rewards for gauge where token is staked
-  /// @param gauge The address of the gauge to claim rewards from
-  function claimGaugeRewards(address gauge) external onlyAdminOrManager {
-    if (gauge == address(0)) revert Invalid0xAddress();
-
-    uint256 nTokens = ILiquidityGauge(gauge).reward_count();
-    address[] memory tokens = new address[](nTokens);
-    uint256[] memory amounts = new uint256[](nTokens);
-
-    for (uint256 i = 0; i < nTokens; i++) {
-      tokens[i] = ILiquidityGauge(gauge).reward_tokens(i);
-      amounts[i] = IERC20(tokens[i]).balanceOf(address(this));
-    }
-
-    ILiquidityGauge(gauge).claim_rewards();
-
-    for (uint256 i = 0; i < nTokens; i++) {
-      amounts[i] = IERC20(tokens[i]).balanceOf(address(this)) - amounts[i];
-
-      emit GaugeRewardsClaimed(gauge, tokens[i], amounts[i]);
-    }
-  }
 }
