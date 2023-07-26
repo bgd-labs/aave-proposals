5e0d4bb02dc2e89bf7367363f8b8e370123eb739
marczeller <pierarte@gmail.com>
2023-07-26 15:06:20 +0200

    test update

--- src/AaveV3_Met_SetEmissionManager_20232607/AaveV3_Met_SetEmissionManager_20232607.t.sol
+++ src/AaveV3_Met_SetEmissionManager_20232607/AaveV3_Met_SetEmissionManager_20232607.t.sol
@@ -5,6 +5,7 @@ import 'forge-std/Test.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {AaveV3Metis, AaveV3MetisAssets} from 'aave-address-book/AaveV3Metis.sol';
+import {IEmissionManager} from 'aave-v3-periphery/rewards/interfaces/IEmissionManager.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3_Met_SetEmissionManager_20232607} from './AaveV3_Met_SetEmissionManager_20232607.sol';
 
@@ -13,6 +14,10 @@ import {AaveV3_Met_SetEmissionManager_20232607} from './AaveV3_Met_SetEmissionMa
  * command: make test-contract filter=AaveV3_Met_SetEmissionManager_20232607
  */
 contract AaveV3_Met_SetEmissionManager_20232607_Test is ProtocolV3TestBase {
+
+  address public constant METIS = AaveV3MetisAssets.Metis_UNDERLYING;
+  address public constant EMISSION_ADMIN = 0x97177cD80475f8b38945c1E77e12F0c9d50Ac84D;
+
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('metis'), 7487447);
   }
@@ -27,14 +32,16 @@ contract AaveV3_Met_SetEmissionManager_20232607_Test is ProtocolV3TestBase {
 
     GovHelpers.executePayload(vm, address(proposal), AaveGovernanceV2.METIS_BRIDGE_EXECUTOR);
 
+    assertEq(IEmissionManager(AaveV3Metis.EMISSION_MANAGER).getEmissionAdmin(METIS), EMISSION_ADMIN);
+
     ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot(
       'postAaveV3_Met_SetEmissionManager_20232607',
       AaveV3Metis.POOL
     );
 
-    e2eTest(
-      AaveV3Metis.POOL
-    );
+
+
+    e2eTest(AaveV3Metis.POOL);
 
     diffReports(
       'preAaveV3_Met_SetEmissionManager_20232607',
