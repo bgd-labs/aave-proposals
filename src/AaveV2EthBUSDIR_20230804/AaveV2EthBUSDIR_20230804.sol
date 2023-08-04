9d8e187dff86b95711dde6c77f3e3a98b8cbe966
marczeller <pierarte@gmail.com>
2023-08-04 18:13:26 +0200

    init

---
+++ src/AaveV2EthBUSDIR_20230804/AaveV2EthBUSDIR_20230804.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import {IERC20} from 'solidity-utils/contracts/oz-common/interfaces/IERC20.sol';
+import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+
+/**
+ * @title [ARFC] BUSD Offboarding Plan part III
+ * @author @marczeller - Aave-Chan Initiative
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x389c0cd79720fd9853ca6714f4597484dd25cc3a5e34955bf6144f0ba1888a3a
+ * - Discussion: https://governance.aave.com/t/arfc-busd-offboarding-plan-part-iii/14136
+ */
+contract AaveV2EthBUSDIR_20230804 is IProposalGenericExecutor {
+  address public constant INTEREST_RATE_STRATEGY = 0x1ffC5d6AA8D46423e45550afeCf8D075DD489C92;
+
+  function execute() external {
+    AaveV2Ethereum.POOL_CONFIGURATOR.setReserveInterestRateStrategyAddress(
+      AaveV2EthereumAssets.BUSD_UNDERLYING,
+      INTEREST_RATE_STRATEGY
+    );
+
+    uint256 aBUSDBalance = IERC20(AaveV2EthereumAssets.BUSD_A_TOKEN).balanceOf(
+      address(AaveV2Ethereum.COLLECTOR)
+    );
+    uint256 availableBUSD = IERC20(AaveV2EthereumAssets.BUSD_UNDERLYING).balanceOf(
+      AaveV2EthereumAssets.BUSD_A_TOKEN
+    );
+    AaveV2Ethereum.COLLECTOR.transfer(
+      AaveV2EthereumAssets.BUSD_A_TOKEN,
+      address(this),
+      aBUSDBalance > availableBUSD ? availableBUSD : aBUSDBalance
+    );
+    AaveV2Ethereum.POOL.withdraw(
+      AaveV2EthereumAssets.BUSD_UNDERLYING,
+      type(uint256).max,
+      address(AaveV2Ethereum.COLLECTOR)
+    );
+  }
+}
---
+++ src/AaveV2EthBUSDIR_20230804/AaveV2EthBUSDIR_20230804Test.t.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import 'forge-std/Test.sol';
+import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {ProtocolV2TestBase, ReserveConfig} from 'aave-helpers/ProtocolV2TestBase.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {AaveV2EthBUSDIR_20230804} from 'src/AaveV2EthBUSDIR_20230804/AaveV2EthBUSDIR_20230804.sol';
+import {IERC20} from 'lib/solidity-utils/src/contracts/oz-common/interfaces/IERC20.sol';
+
+/**
+ * @dev Test for AaveV2EthBUSDIR_20230804
+ * command: make test-contract filter=AaveV2EthBUSDIR_20230804_Test
+ */
+
+contract AaveV2EthBUSDIR_20230804_Test is ProtocolV2TestBase {
+  address public constant BUSD = AaveV2EthereumAssets.BUSD_UNDERLYING;
+  string public constant BUSD_SYMBOL = 'BUSD';
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'), 17842592);
+  }
+
+  function testBUSD() public {
+    ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot(
+      'pre-BUSD-Payload-activation_20230804',
+      AaveV2Ethereum.POOL
+    );
+
+    ReserveConfig memory configBUSDBefore = _findReserveConfigBySymbol(
+      allConfigsBefore,
+      BUSD_SYMBOL
+    );
+
+    address BUSDPayload = address(new AaveV2EthBUSDIR_20230804());
+
+    uint256 aBUSDBalanceBefore = IERC20(AaveV2EthereumAssets.BUSD_A_TOKEN).balanceOf(
+      address(AaveV2Ethereum.COLLECTOR)
+    );
+    uint256 BUSDBalanceBefore = IERC20(AaveV2EthereumAssets.BUSD_UNDERLYING).balanceOf(
+      address(AaveV2Ethereum.COLLECTOR)
+    );
+
+    GovHelpers.executePayload(vm, BUSDPayload, AaveGovernanceV2.SHORT_EXECUTOR);
+
+    // check balances are correct
+    uint256 aBUSDBalanceAfter = IERC20(AaveV2EthereumAssets.BUSD_A_TOKEN).balanceOf(
+      address(AaveV2Ethereum.COLLECTOR)
+    );
+    uint256 BUSDBalanceAfter = IERC20(AaveV2EthereumAssets.BUSD_UNDERLYING).balanceOf(
+      address(AaveV2Ethereum.COLLECTOR)
+    );
+    assertApproxEqAbs(aBUSDBalanceAfter, 0, 1500 ether, 'aBUSD_LEFTOVER');
+    assertEq(BUSDBalanceAfter, aBUSDBalanceBefore + BUSDBalanceBefore);
+    ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot(
+      'post-BUSD-Payload-activation_20230804',
+      AaveV2Ethereum.POOL
+    );
+
+    // check it's not bricked
+    ReserveConfig memory configBUSDAfter = _findReserveConfigBySymbol(allConfigsAfter, BUSD_SYMBOL);
+    _withdraw(
+      configBUSDAfter,
+      AaveV2Ethereum.POOL,
+      0xc579a79376148c4B17821C5Eb9434965f3a15C80,
+      1 ether
+    ); // aBUSD whale
+
+    // check there are no unexpected changes
+    _noReservesConfigsChangesApartFrom(
+      allConfigsBefore,
+      allConfigsAfter,
+      configBUSDBefore.underlying
+    );
+
+    diffReports('pre-BUSD-Payload-activation_20230804', 'post-BUSD-Payload-activation_20230804');
+  }
+}
