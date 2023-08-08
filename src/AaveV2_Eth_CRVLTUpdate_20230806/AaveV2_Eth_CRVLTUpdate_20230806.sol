16cd746e98e42e4ddd83c4918de480eb5e537121
Omer <arieg419@gmail.com>
2023-08-08 11:31:36 +0300

    update forum link

--- src/AaveV2_Eth_CRVLTUpdate_20230806/AaveV2_Eth_CRVLTUpdate_20230806.sol
+++ src/AaveV2_Eth_CRVLTUpdate_20230806/AaveV2_Eth_CRVLTUpdate_20230806.sol
@@ -9,7 +9,7 @@ import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethe
  * @title Chaos Labs Risk Parameter Updates - CRV Aave V2 Ethereum
  * @author @omeragoldberg - ChaosLabsInc
  * - Snapshot: https://snapshot.org/\#/aave.eth/proposal/0x8b992ee05d9e87ef0dab2cb7178c24f7b4b6f5d79561ad33298550b3c8d9fe89
- * - Discussion: https://snapshot.org/\#/aave.eth/proposal/0x8b992ee05d9e87ef0dab2cb7178c24f7b4b6f5d79561ad33298550b3c8d9fe89
+ * - Discussion: https://governance.aave.com/t/post-vyper-exploit-crv-market-update-and-recommendations/14214/47
  */
 contract AaveV2_Eth_CRVLTUpdate_20230806 is AaveV2PayloadEthereum {
     uint256 public constant CRV_LIQUIDATION_THRESHOLD = 49_00; // 55 -> 49
