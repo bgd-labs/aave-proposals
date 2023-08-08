710eeb7c856af0b5b63e43abdec37f5f7fccbde7
Omer <arieg419@gmail.com>
2023-08-08 19:01:24 +0300

    update contract inheritance for to reduce codesize

--- src/AaveV2_Eth_CRVLTUpdate_20230806/AaveV2_Eth_CRVLTUpdate_20230806.sol
+++ src/AaveV2_Eth_CRVLTUpdate_20230806/AaveV2_Eth_CRVLTUpdate_20230806.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV2PayloadEthereum} from 'aave-helpers/v2-config-engine/AaveV2PayloadEthereum.sol';
 import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 
 
 /**
@@ -11,12 +11,12 @@ import {AaveV2Ethereum, AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethe
  * - Snapshot: https://snapshot.org/\#/aave.eth/proposal/0x8b992ee05d9e87ef0dab2cb7178c24f7b4b6f5d79561ad33298550b3c8d9fe89
  * - Discussion: https://governance.aave.com/t/post-vyper-exploit-crv-market-update-and-recommendations/14214/47
  */
-contract AaveV2_Eth_CRVLTUpdate_20230806 is AaveV2PayloadEthereum {
+contract AaveV2_Eth_CRVLTUpdate_20230806 is IProposalGenericExecutor{
     uint256 public constant CRV_LIQUIDATION_THRESHOLD = 49_00; // 55 -> 49
     uint256 public constant CRV_LTV = 0; // unchanged 
     uint256 public constant CRV_LIQUIDATION_BONUS = 10800; // unchanged
 
-    function _postExecute() internal override {
+    function execute() external {
         AaveV2Ethereum.POOL_CONFIGURATOR.configureReserveAsCollateral(
             AaveV2EthereumAssets.CRV_UNDERLYING,
             CRV_LTV,
