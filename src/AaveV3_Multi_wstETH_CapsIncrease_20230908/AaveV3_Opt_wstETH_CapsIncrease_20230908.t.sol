5907a2754ead894b23de82086e01ee7a0edbeebd
Alice <alice@rozengarden.eu>
2023-08-09 22:27:35 +0200

    Add test and test result for Optimism

--- src/AaveV3_Multi_wstETH_CapsIncrease_20230908/AaveV3_Opt_wstETH_CapsIncrease_20230908.t.sol
+++ src/AaveV3_Multi_wstETH_CapsIncrease_20230908/AaveV3_Opt_wstETH_CapsIncrease_20230908.t.sol
@@ -14,7 +14,7 @@ import {AaveV3_Opt_wstETH_CapsIncrease_20230908} from './AaveV3_Opt_wstETH_CapsI
  */
 contract AaveV3_Opt_wstETH_CapsIncrease_20230908_Test is ProtocolV3TestBase {
   function setUp() public {
-    vm.createSelectFork(vm.rpcUrl('optimism'), 107994763);
+    vm.createSelectFork(vm.rpcUrl('optimism'), 108006761);
   }
 
   function testProposalExecution() public {
@@ -35,6 +35,26 @@ contract AaveV3_Opt_wstETH_CapsIncrease_20230908_Test is ProtocolV3TestBase {
       'postAaveV3_Opt_wstETH_CapsIncrease_20230908',
       AaveV3Optimism.POOL
     );
+
+    ReserveConfig memory wstETH = _findReserveConfig(
+      allConfigsBefore,
+      AaveV3OptimismAssets.wstETH_UNDERLYING
+    );
+
+    wstETH.supplyCap = proposal.NEW_SUPPLY_CAP();
+
+    address[] memory assetsChanged = new address[](1);
+    assetsChanged[0] = AaveV3OptimismAssets.wstETH_UNDERLYING;
+
+    _noReservesConfigsChangesApartFrom(allConfigsBefore, allConfigsAfter, assetsChanged);
+
+    _validateReserveConfig(wstETH, allConfigsAfter);
+
+    e2eTestAsset(
+        AaveV3Optimism.POOL,
+        _findReserveConfig(allConfigsAfter, AaveV3OptimismAssets.WETH_UNDERLYING),
+        _findReserveConfig(allConfigsAfter, AaveV3OptimismAssets.wstETH_UNDERLYING)
+        );
 
     diffReports('preAaveV3_Opt_wstETH_CapsIncrease_20230908', 'postAaveV3_Opt_wstETH_CapsIncrease_20230908');
   }
