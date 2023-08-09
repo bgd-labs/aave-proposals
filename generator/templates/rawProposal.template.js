215d3a753fdcb8e56d41c051ba7ba5baf4d11b93
Zer0dot <zer0dot.dev@gmail.com>
2023-08-09 17:19:58 -0400

    misc: Remove unnecessary line breaks

--- generator/templates/rawProposal.template.js
+++ generator/templates/rawProposal.template.js
@@ -16,7 +16,6 @@ pragma solidity ^0.8.0;
 import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
 import {Aave${protocolVersion}${chain}, Aave${protocolVersion}${chain}Assets} from 'aave-address-book/Aave${protocolVersion}${chain}.sol';
 
-
 /**
  * @title ${title || "TODO"}
  * @author ${author || "TODO"}
