f10981b8c3a9a178d3ea4e75acec8fd19774982d
Omer <arieg419@gmail.com>
2023-08-09 11:27:39 +0300

    update proposal payload address

--- src/AaveV3_Multi_DisableCRVBorrows_20230508/AaveV3_Multi_DisableCRVBorrows_20230508.s.sol
+++ src/AaveV3_Multi_DisableCRVBorrows_20230508/AaveV3_Multi_DisableCRVBorrows_20230508.s.sol
@@ -33,8 +33,8 @@ contract DeployPolygon is PolygonScript {
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](2);
-    payloads[0] = GovHelpers.buildMainnet(address(0));
-    payloads[1] = GovHelpers.buildPolygon(address(0));
+    payloads[0] = GovHelpers.buildMainnet(address(0x10bDbDEB8D3Ff6273261dD7E9B90465000325DcB));
+    payloads[1] = GovHelpers.buildPolygon(address(0xFa6481b09c273d17701fb90427D6658a028EdC18));
     GovHelpers.createProposal(payloads, GovHelpers.ipfsHashFile(vm, 'src/AaveV3_Multi_DisableCRVBorrows_20230508/DisableCRVBorrows.md'));
   }
 }
