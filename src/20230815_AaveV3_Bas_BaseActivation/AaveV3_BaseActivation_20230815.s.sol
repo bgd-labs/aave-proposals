7229b02e26d0ff07dd2df02c124cb77b923158b0
Rustem Kurmaev <rustboyar@gmail.com>
2023-08-15 18:51:33 +0200

    add payload address

--- src/20230815_AaveV3_Bas_BaseActivation/AaveV3_BaseActivation_20230815.s.sol
+++ src/20230815_AaveV3_Bas_BaseActivation/AaveV3_BaseActivation_20230815.s.sol
@@ -11,7 +11,7 @@ import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
-    payloads[0] = GovHelpers.buildBase(address(0));
+    payloads[0] = GovHelpers.buildBase(0xBdD1458A6d399C88D4509275e4463485C6c86Ef3);
     GovHelpers.createProposal(
       payloads,
       GovHelpers.ipfsHashFile(vm, 'src/20230815_AaveV3_Bas_BaseActivation/BaseActivation.md')
