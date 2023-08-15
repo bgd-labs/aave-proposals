669718a7b5e55af045cc483c678e590a560478a5
Lukas <lukasstrassel@googlemail.com>
2023-08-15 10:44:03 +0200

    test: check if admin was moved

--- src/20230815_AaveV3_Multi_StataTokenOperationalUpdate/AaveV3_Avalanche_StataTokenOperationalUpdate_20230815.t.sol
+++ src/20230815_AaveV3_Multi_StataTokenOperationalUpdate/AaveV3_Avalanche_StataTokenOperationalUpdate_20230815.t.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.0;
 import 'forge-std/Test.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
-import {AaveV3Avalanche, AaveV3AvalancheAssets} from 'aave-address-book/AaveV3Avalanche.sol';
+import {AaveV3Avalanche} from 'aave-address-book/AaveV3Avalanche.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3_Avalanche_StataTokenOperationalUpdate_20230815} from './AaveV3_Avalanche_StataTokenOperationalUpdate_20230815.sol';
+import {AaveMisc} from 'aave-address-book/AaveMisc.sol';
+import {ProxyAdmin} from 'solidity-utils/contracts/transparent-proxy/ProxyAdmin.sol';
+import {TransparentUpgradeableProxy} from 'solidity-utils/contracts/transparent-proxy/TransparentUpgradeableProxy.sol';
+import {IStaticATokenFactory} from './IStaticATokenFactory.sol';
 
 /**
  * @dev Test for AaveV3_Avalanche_StataTokenOperationalUpdate_20230815
@@ -26,5 +30,21 @@ contract AaveV3_Avalanche_StataTokenOperationalUpdate_20230815_Test is ProtocolV
       address(proposal),
       0xa35b76E4935449E33C56aB24b23fcd3246f13470 // avalanche guardian
     );
+    // trying to swap back admin
+    address[] memory tokens = IStaticATokenFactory(AaveV3Avalanche.STATIC_A_TOKEN_FACTORY)
+      .getStaticATokens();
+    address newAdmin = proposal.NEW_ADMIN();
+    vm.startPrank(newAdmin);
+    for (uint256 i; i < tokens.length; i++) {
+      assertEq(TransparentUpgradeableProxy(payable(tokens[i])).admin(), newAdmin);
+      TransparentUpgradeableProxy(payable(tokens[i])).changeAdmin(AaveMisc.PROXY_ADMIN_AVALANCHE);
+    }
+    vm.startPrank(AaveMisc.PROXY_ADMIN_AVALANCHE);
+    for (uint256 i; i < tokens.length; i++) {
+      assertEq(
+        TransparentUpgradeableProxy(payable(tokens[i])).admin(),
+        AaveMisc.PROXY_ADMIN_AVALANCHE
+      );
+    }
   }
 }
--- src/20230815_AaveV3_Multi_StataTokenOperationalUpdate/AaveV3_Polygon_StataTokenOperationalUpdate_20230815.t.sol
+++ src/20230815_AaveV3_Multi_StataTokenOperationalUpdate/AaveV3_Polygon_StataTokenOperationalUpdate_20230815.t.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.0;
 import 'forge-std/Test.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
-import {AaveV3Polygon, AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
+import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3_Polygon_StataTokenOperationalUpdate_20230815} from './AaveV3_Polygon_StataTokenOperationalUpdate_20230815.sol';
+import {AaveMisc} from 'aave-address-book/AaveMisc.sol';
+import {ProxyAdmin} from 'solidity-utils/contracts/transparent-proxy/ProxyAdmin.sol';
+import {TransparentUpgradeableProxy} from 'solidity-utils/contracts/transparent-proxy/TransparentUpgradeableProxy.sol';
+import {IStaticATokenFactory} from './IStaticATokenFactory.sol';
 
 /**
  * @dev Test for AaveV3_Polygon_StataTokenOperationalUpdate_20230815
@@ -22,5 +26,21 @@ contract AaveV3_Polygon_StataTokenOperationalUpdate_20230815_Test is ProtocolV3T
 
   function testProposalExecution() public {
     GovHelpers.executePayload(vm, address(proposal), AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR);
+    // trying to swap back admin
+    address[] memory tokens = IStaticATokenFactory(AaveV3Polygon.STATIC_A_TOKEN_FACTORY)
+      .getStaticATokens();
+    address newAdmin = proposal.NEW_ADMIN();
+    vm.startPrank(newAdmin);
+    for (uint256 i; i < tokens.length; i++) {
+      assertEq(TransparentUpgradeableProxy(payable(tokens[i])).admin(), newAdmin);
+      TransparentUpgradeableProxy(payable(tokens[i])).changeAdmin(AaveMisc.PROXY_ADMIN_POLYGON);
+    }
+    vm.startPrank(AaveMisc.PROXY_ADMIN_POLYGON);
+    for (uint256 i; i < tokens.length; i++) {
+      assertEq(
+        TransparentUpgradeableProxy(payable(tokens[i])).admin(),
+        AaveMisc.PROXY_ADMIN_POLYGON
+      );
+    }
   }
 }
