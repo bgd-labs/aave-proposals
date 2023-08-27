4d998a2e2164da23fc7391fbdb35961882e05b05
Natanela <37778340+Natanela@users.noreply.github.com>
2023-08-27 11:41:35 +0300

    Update src/AaveV3_Opt_RiskParamsUpdate_20232408/AaveV3_Opt_RiskParamsUpdate_20232408.sol
    Co-authored-by: yonikesel1 <49488355+yonikesel1@users.noreply.github.com>

--- src/AaveV3_Opt_RiskParamsUpdate_20232408/AaveV3_Opt_RiskParamsUpdate_20232408.sol
+++ src/AaveV3_Opt_RiskParamsUpdate_20232408/AaveV3_Opt_RiskParamsUpdate_20232408.sol
@@ -8,7 +8,7 @@ import {AaveV3PayloadOptimism, IEngine, EngineFlags} from 'aave-helpers/v3-confi
  * @title Chaos Labs Risk Parameter Updates - Aave V3 Optimism - 2023.08.13
  * @author Chaos Labs
  * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0x7568e17d2d7078686255a8fadf563e1f072abae0b79188a5b5b76852a6ebd63f
- * - Discussion: https://governance.aave.com/t/arfc-chaos-labs-risk-parameter-updates-aave-v3-optimism-2023-08-13/14466/1
+ * - Discussion: https://governance.aave.com/t/arfc-chaos-labs-risk-parameter-updates-aave-v3-optimism-2023-08-13/14466
  */
 contract AaveV3_Opt_RiskParamsUpdate_20232408 is AaveV3PayloadOptimism {
   function collateralsUpdates() public pure override returns (IEngine.CollateralUpdate[] memory) {
