efcaa814fc1e8390d6e752dfaae3f2ed5a5a9562
eboado <ebdmrr@gmail.com>
2023-08-30 11:58:40 +0200

    fix: robot v2 activation script

--- src/20230817_AaveRobotActivation/AaveRobotActivation_20230817.s.sol
+++ src/20230817_AaveRobotActivation/AaveRobotActivation_20230817.s.sol
@@ -12,9 +12,9 @@ contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](4);
     payloads[0] = GovHelpers.buildMainnet(0x8aD3f00e91F0a3Ad8b0dF897c19EC345EaB761c4);
-    payloads[0] = GovHelpers.buildOptimism(0x67F0B21FB75B88a7553039631A2Bc796Fb89e0A4);
-    payloads[0] = GovHelpers.buildArbitrum(0xdDE20B20E21a6F3b7080e740b684CDf5b764B80D);
-    payloads[0] = GovHelpers.buildPolygon(0xdDE20B20E21a6F3b7080e740b684CDf5b764B80D);
+    payloads[1] = GovHelpers.buildOptimism(0x67F0B21FB75B88a7553039631A2Bc796Fb89e0A4);
+    payloads[2] = GovHelpers.buildArbitrum(0xdDE20B20E21a6F3b7080e740b684CDf5b764B80D);
+    payloads[3] = GovHelpers.buildPolygon(0xdDE20B20E21a6F3b7080e740b684CDf5b764B80D);
     GovHelpers.createProposal(payloads, GovHelpers.ipfsHashFile(vm, 'src/20230817_AaveRobotActivation/AaveRobotActivation.md'));
   }
 }
