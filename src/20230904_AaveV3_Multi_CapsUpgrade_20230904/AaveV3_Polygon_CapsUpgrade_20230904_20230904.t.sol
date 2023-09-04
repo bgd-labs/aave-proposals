c8729474a51555e04c579abf9d1a06a24cabb144
Rozengarden <github@rozengarden.eu>
2023-09-04 16:16:48 +0200

    added test & test result for polygon
    e2easset failing at _repay() for allConfigAfter and allConfigBefore with DPI as borrowed (other supply/borrow pair worked)

--- src/20230904_AaveV3_Multi_CapsUpgrade_20230904/AaveV3_Polygon_CapsUpgrade_20230904_20230904.t.sol
+++ src/20230904_AaveV3_Multi_CapsUpgrade_20230904/AaveV3_Polygon_CapsUpgrade_20230904_20230904.t.sol
@@ -33,6 +33,23 @@ contract AaveV3_Polygon_CapsUpgrade_20230904_20230904_Test is ProtocolV3TestBase
       AaveV3Polygon.POOL
     );
 
+    address[] memory assetsChanged = new address[](1);
+    assetsChanged[0] = AaveV3PolygonAssets.DPI_UNDERLYING;
+    _noReservesConfigsChangesApartFrom(allConfigsBefore, allConfigsAfter, assetsChanged);
+
+    ReserveConfig memory DPI = _findReserveConfig(
+      allConfigsBefore,
+      AaveV3PolygonAssets.DPI_UNDERLYING
+    );
+    DPI.supplyCap = 2_460;
+    _validateReserveConfig(DPI, allConfigsAfter);
+
+//     e2eTestAsset(
+//        AaveV3Polygon.POOL,
+//        _findReserveConfig(allConfigsAfter, AaveV3PolygonAssets.WMATIC_UNDERLYING),
+//        _findReserveConfig(allConfigsAfter, AaveV3PolygonAssets.DPI_UNDERLYING)
+//        );
+
     diffReports(
       'preAaveV3_Polygon_CapsUpgrade_20230904_20230904',
       'postAaveV3_Polygon_CapsUpgrade_20230904_20230904'
