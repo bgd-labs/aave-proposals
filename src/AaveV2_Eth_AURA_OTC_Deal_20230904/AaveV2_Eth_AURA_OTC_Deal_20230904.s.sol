7e56763108af74bc7a3895ed13321efa51a90c41
marczeller <pierarte@gmail.com>
2023-09-08 13:37:10 +0200

    payload deployed

--- src/AaveV2_Eth_AURA_OTC_Deal_20230904/AaveV2_Eth_AURA_OTC_Deal_20230904.s.sol
+++ src/AaveV2_Eth_AURA_OTC_Deal_20230904/AaveV2_Eth_AURA_OTC_Deal_20230904.s.sol
@@ -7,7 +7,7 @@ import {AaveV2_Eth_AURA_OTC_Deal_20230508} from 'src/AaveV2_Eth_AURA_OTC_Deal_20
 
 /**
  * @dev Deploy AaveV2_Eth_AURA_OTC_Deal_20230508
- * command: make deploy-ledger contract=src/AaveV2_Eth_AURA_OTC_Deal_20230508/AaveV2_Eth_AURA_OTC_Deal_20230508.s.sol:DeployEthereum chain=mainnet
+ * command: make deploy-ledger contract=src/AaveV2_Eth_AURA_OTC_Deal_20230904/AaveV2_Eth_AURA_OTC_Deal_20230904.s.sol:DeployEthereum chain=mainnet
  */
 contract DeployEthereum is EthereumScript {
   function run() external broadcast {
@@ -17,12 +17,12 @@ contract DeployEthereum is EthereumScript {
 
 /**
  * @dev Create Proposal
- * command: make deploy-ledger contract=src/AaveV2_Eth_AURA_OTC_Deal_20230508/AaveV2_Eth_AURA_OTC_Deal_20230508.s.sol:CreateProposal chain=mainnet
+ * command: make deploy-ledger contract=src/AaveV2_Eth_AURA_OTC_Deal_20230904/AaveV2_Eth_AURA_OTC_Deal_20230904.s.sol:CreateProposal chain=mainnet
  */
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
-    payloads[0] = GovHelpers.buildMainnet(0x8eDa0756f0d7DffC8488A19cF3B949BEbD132191);
+    payloads[0] = GovHelpers.buildMainnet(0xdFeE3eC490cAaBf760f77b39652E4208A5848971);
     GovHelpers.createProposal(
       payloads,
       GovHelpers.ipfsHashFile(vm, 'src/AaveV2_Eth_AURA_OTC_Deal_20230904/AURA_OTC_Deal.md')
