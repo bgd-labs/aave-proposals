1cf39d01990c33f396314c40ce4dc60f63f8d170
Rozengarden <github@rozengarden.eu>
2023-09-08 14:22:10 +0200

    init from generator & smoll optimization

---
+++ src/20230908_AaveV3_Eth_MKRDebtCeiling/AaveV3_Ethereum_MKRDebtCeiling_20230908.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AaveV3PayloadEthereum, IEngine, EngineFlags} from 'aave-helpers/v3-config-engine/AaveV3PayloadEthereum.sol';
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+
+/**
+ * @title MKRDebtCeiling
+ * @author Alice Rozengarden (@Rozengarden - Aave-chan initiative)
+ * - Snapshot: No snapshot for Direct-to-AIP
+ * - Discussion: https://governance.aave.com/t/arfc-borrowing-cap-expansion-request-mkr/14763
+ */
+contract AaveV3_Ethereum_MKRDebtCeiling_20230908 is AaveV3PayloadEthereum {
+  function _preExecute() internal override {}
+
+  function capsUpdates() public pure override returns (IEngine.CapsUpdate[] memory) {
+    IEngine.CollateralUpdate[] memory collateralUpdate = new IEngine.CollateralUpdate[](1);
+
+    capsUpdate[0] = IEngine.CapsUpdate({
+      asset: AaveV3EthereumAssets.MKR_UNDERLYING,
+      ltv: EngineFlags.KEEP_CURRENT,
+      liqThreshold: EngineFlags.KEEP_CURRENT,
+      liqBonus: EngineFlags.KEEP_CURRENT,
+      debtCeiling: 6_000_000,
+      liqProtocolFee: EngineFlags.KEEP_CURRENT,
+      eModeCategory: KEEP_CURRENT
+    });
+
+    return capsUpdate;
+  }
+}
---
+++ src/20230908_AaveV3_Eth_MKRDebtCeiling/MKRDebtCeiling.md
@@ -0,0 +1,22 @@
+---
+title: "MKRDebtCeiling"
+author: "Alice Rozengarden (@Rozengarden - Aave-chan initiative)"
+discussions: "https://governance.aave.com/t/arfc-borrowing-cap-expansion-request-mkr/14763"
+---
+
+## Simple Summary
+
+## Motivation
+
+## Specification
+
+## References
+
+- Implementation: [Ethereum](https://github.com/bgd-labs/aave-proposals/blob/main/src/20230908_AaveV3_Eth_MKRDebtCeiling/AaveV3_Ethereum_MKRDebtCeiling_20230908.sol)
+- Tests: [Ethereum](https://github.com/bgd-labs/aave-proposals/blob/main/src/20230908_AaveV3_Eth_MKRDebtCeiling/AaveV3_Ethereum_MKRDebtCeiling_20230908.t.sol)
+- [Snapshot](No snapshot for Direct-to-AIP)
+- [Discussion](https://governance.aave.com/t/arfc-borrowing-cap-expansion-request-mkr/14763)
+
+## Copyright
+
+Copyright and related rights waived via [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
