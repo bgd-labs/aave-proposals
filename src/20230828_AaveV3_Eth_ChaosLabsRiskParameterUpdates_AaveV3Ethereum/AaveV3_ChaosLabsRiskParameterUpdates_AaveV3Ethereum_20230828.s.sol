623a642d96bf69ced320c390763b873bdee8f72f
yoavigra <yoavigra@gmail.com>
2023-09-12 16:08:14 +0300

    Chaos Labs Risk Parameter Updates - Aave V3 Ethereum (#286)
    * Chaos Labs Risk Parameter Updates - Aave V3 Ethereum
    
    * prettier
    
    * Use e2eTest instead of e2eTestAsset
    
    * Update snapshot url
    
    * fix typo
    
    * Update payload contract address
    
    ---------
    
    Co-authored-by: Yoav Igra <yoav@chaoslabs.xyz>

---
+++ src/20230828_AaveV3_Eth_ChaosLabsRiskParameterUpdates_AaveV3Ethereum/AaveV3_ChaosLabsRiskParameterUpdates_AaveV3Ethereum_20230828.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
+import {AaveV3_Ethereum_ChaosLabsRiskParameterUpdates_AaveV3Ethereum_20230828} from './AaveV3_Ethereum_ChaosLabsRiskParameterUpdates_AaveV3Ethereum_20230828.sol';
+
+/**
+ * @dev Deploy AaveV3_Ethereum_ChaosLabsRiskParameterUpdates_AaveV3Ethereum_20230828
+ * command: make deploy-ledger contract=src/20230828_AaveV3_Eth_ChaosLabsRiskParameterUpdates_AaveV3Ethereum/AaveV3_ChaosLabsRiskParameterUpdates_AaveV3Ethereum_20230828.s.sol:DeployEthereum chain=mainnet
+ */
+contract DeployEthereum is EthereumScript {
+  function run() external broadcast {
+    new AaveV3_Ethereum_ChaosLabsRiskParameterUpdates_AaveV3Ethereum_20230828();
+  }
+}
+
+/**
+ * @dev Create Proposal
+ * command: make deploy-ledger contract=src/20230828_AaveV3_Eth_ChaosLabsRiskParameterUpdates_AaveV3Ethereum/AaveV3_ChaosLabsRiskParameterUpdates_AaveV3Ethereum_20230828.s.sol:CreateProposal chain=mainnet
+ */
+contract CreateProposal is EthereumScript {
+  function run() external broadcast {
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
+    payloads[0] = GovHelpers.buildMainnet(0xc52B8360187aE6383466df74c6Cba8cF0cA790D1);
+    GovHelpers.createProposal(
+      payloads,
+      GovHelpers.ipfsHashFile(
+        vm,
+        'src/20230828_AaveV3_Eth_ChaosLabsRiskParameterUpdates_AaveV3Ethereum/ChaosLabsRiskParameterUpdates_AaveV3Ethereum.md'
+      )
+    );
+  }
+}
