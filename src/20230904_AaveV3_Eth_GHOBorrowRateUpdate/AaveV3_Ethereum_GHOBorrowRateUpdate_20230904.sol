b9cf4e76555cea59a47636cdc3046fc12a1ecf5b
Marc Zeller <pierarte@gmail.com>
2023-09-15 13:31:27 +0200

    Update src/20230904_AaveV3_Eth_GHOBorrowRateUpdate/AaveV3_Ethereum_GHOBorrowRateUpdate_20230904.sol
    Co-authored-by: Harsh Pandey <harshsatishpandey@gmail.com>

--- src/20230904_AaveV3_Eth_GHOBorrowRateUpdate/AaveV3_Ethereum_GHOBorrowRateUpdate_20230904.sol
+++ src/20230904_AaveV3_Eth_GHOBorrowRateUpdate/AaveV3_Ethereum_GHOBorrowRateUpdate_20230904.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3Ethereum, AaveV3EthereumAssets} from 'lib/aave-address-book/src/AaveV3Ethereum.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 
 /**
  * @title GHO Borrow Rate Update
