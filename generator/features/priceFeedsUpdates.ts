56c533b41003de056b3c3f68654e608c908150b2
sakulstra <lukasstrassel@googlemail.com>
2023-09-21 17:02:49 +0200

    feat: allow price feed updates(unfinished)

---
+++ generator/features/priceFeedsUpdates.ts
@@ -0,0 +1,62 @@
+import {CodeArtifact, DEPENDENCIES, FeatureModule, PoolIdentifier} from '../types';
+import {NumberInputValues, assetsSelect, numberInput} from '../prompts';
+
+async function subCli(pool: PoolIdentifier) {
+  console.log(`Fetching information for PriceFeeds on ${pool}`);
+  const assets = await assetsSelect({
+    message: 'Select the assets you want to amend',
+    pool,
+  });
+  const answers: PriceFeedUpdate[] = [];
+  for (const asset of assets) {
+    console.log(`collecting info for ${asset}`);
+    answers.push({
+      asset,
+      priceFeed: await numberInput({
+        message: 'New price feed',
+      }),
+    });
+  }
+  return answers;
+}
+
+type PriceFeedUpdate = {
+  asset: string;
+  priceFeed: NumberInputValues;
+};
+
+type PriceFeedUpdates = PriceFeedUpdate[];
+
+export const capsUpdates: FeatureModule<PriceFeedUpdates> = {
+  value: 'PriceFeedsUpdates (replacing priceFeeds)',
+  async cli(opt, pool) {
+    const response: PriceFeedUpdates = await subCli(pool);
+    return response;
+  },
+  build(opt, pool, cfg) {
+    const response: CodeArtifact = {
+      code: {
+        dependencies: [DEPENDENCIES.Assets, DEPENDENCIES.Engine],
+        fn: [
+          `function priceFeedsUpdates() public pure override returns (IEngine.PriceFeedUpdate[] memory) {
+          IEngine.PriceFeedUpdate[] memory priceFeedsUpdates = new IEngine.PriceFeedUpdate[](${
+            cfg.length
+          });
+
+          ${cfg
+            .map(
+              (cfg, ix) => `priceFeedsUpdates[${ix}] = IEngine.PriceFeedUpdate({
+               asset: ${pool}Assets.${cfg.asset}_UNDERLYING,
+               priceFeed: ${cfg.priceFeed}
+             });`
+            )
+            .join('\n')}
+
+          return priceFeedsUpdates;
+        }`,
+        ],
+      },
+    };
+    return response;
+  },
+};
