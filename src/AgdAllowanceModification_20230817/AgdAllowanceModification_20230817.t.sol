7d1d0827276e802ebd6a39a11c0bcf384899a8e0
defijesus.eth <defijesus@buzzedbearhideout.com>
2023-09-22 18:35:42 +0100

    improve comments

--- src/AgdAllowanceModification_20230817/AgdAllowanceModification_20230817.t.sol
+++ src/AgdAllowanceModification_20230817/AgdAllowanceModification_20230817.t.sol
@@ -20,15 +20,16 @@ contract AgdAllowanceModification_20230817_Test is ProtocolV2TestBase {
     address AGD_MULTISIG = 0x89C51828427F70D77875C6747759fB17Ba10Ceb0;
 
     // milkman creates intermediary contract for each swap
-    // while swap is not executed the assets will be in these addresses instead of aaveSwapper;
+    // while swap is not executed the assets will be in these swap-specific proxy addresses instead of aaveSwapper
+    // proxy contracts addresses are deterministic, they could be derived via code. 
+    // I simulated execution and copy pasted the address for simplicity
+    // see https://etherscan.io/address/0x11C76AD590ABDFFCD980afEC9ad951B160F02797#code#L878
     address usdcMilkmanCreatedContract = 0x2414B7eDd549E62e8a5877b73D96C80bAbC30bca;
     address usdtMilkmanCreatedContract = 0x86487dad62c99A37d0052ed56BF1EafF2959294D;
 
     uint256 usdcAmount = 228_000 * 1e6;
     uint256 usdtAmount = 150_000 * 1e6;
 
-    uint256 usdcBalanceBefore = IERC20(AaveV3EthereumAssets.USDC_UNDERLYING).balanceOf(AaveMisc.AAVE_SWAPPER_ETHEREUM);
-    uint256 usdtBalanceBefore = IERC20(AaveV3EthereumAssets.USDT_UNDERLYING).balanceOf(AaveMisc.AAVE_SWAPPER_ETHEREUM);
     uint256 aDaiAllowanceBefore = IERC20(AaveV2EthereumAssets.DAI_A_TOKEN)
       .allowance(address(AaveV2Ethereum.COLLECTOR), AGD_MULTISIG);
 
@@ -36,12 +37,15 @@ contract AgdAllowanceModification_20230817_Test is ProtocolV2TestBase {
 
     GovHelpers.executePayload(vm, address(payload), AaveGovernanceV2.SHORT_EXECUTOR);
 
-    /// check if swapper got the assets to swap
+    /// check if proxy contracts got the assets to swap
     uint256 usdcBalanceAfter = IERC20(AaveV3EthereumAssets.USDC_UNDERLYING).balanceOf(usdcMilkmanCreatedContract);
     uint256 usdtBalanceAfter = IERC20(AaveV3EthereumAssets.USDT_UNDERLYING).balanceOf(usdtMilkmanCreatedContract);
 
-    assertEq(usdcBalanceAfter, usdcBalanceBefore + usdcAmount);
-    assertEq(usdtBalanceAfter, usdtBalanceBefore + usdtAmount);
+
+    /// aaveSwapper never really holds any assets that are earmarked for a swap
+    /// the swap function creates a proxy contract for each swap, and those proxies hold the assets waiting to be swapped
+    assertEq(usdcBalanceAfter, usdcAmount);
+    assertEq(usdtBalanceAfter, usdtAmount);
 
     /// check allowances are correctly set
 
