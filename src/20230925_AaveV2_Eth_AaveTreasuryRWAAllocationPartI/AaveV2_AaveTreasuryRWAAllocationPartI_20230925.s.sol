898aca09e21002131f2cefc3ee9bc8bc08d30eb4
marczeller <pierarte@gmail.com>
2023-09-29 15:42:53 +0200

    Payload deployed

--- src/20230925_AaveV2_Eth_AaveTreasuryRWAAllocationPartI/AaveV2_AaveTreasuryRWAAllocationPartI_20230925.s.sol
+++ src/20230925_AaveV2_Eth_AaveTreasuryRWAAllocationPartI/AaveV2_AaveTreasuryRWAAllocationPartI_20230925.s.sol
@@ -22,7 +22,7 @@ contract DeployEthereum is EthereumScript {
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
-    payloads[0] = GovHelpers.buildMainnet(address(0));
+    payloads[0] = GovHelpers.buildMainnet(0xfD6933AEDC23FcbA8B52dE85D2abfe6C93EF6f75);
     GovHelpers.createProposal(
       payloads,
       GovHelpers.ipfsHashFile(
