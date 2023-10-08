3710e418721c7217a102ef34c360c86634f0a4a6
Rozengarden <github@rozengarden.eu>
2023-10-08 20:04:51 +0200

    Payload code

--- src/20231008_AaveV3_Eth_STGOnboardingOnAaveV3EthereumMarket/AaveV3_Ethereum_STGOnboardingOnAaveV3EthereumMarket_20231008.sol
+++ src/20231008_AaveV3_Eth_STGOnboardingOnAaveV3EthereumMarket/AaveV3_Ethereum_STGOnboardingOnAaveV3EthereumMarket_20231008.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGenericExecutor.sol';
+import {AaveV3PayloadEthereum, IEngine, Rates, EngineFlags} from 'lib/aave-helpers/src/v3-config-engine/AaveV3PayloadEthereum.sol';
 
 /**
  * @title STG onboarding on AaveV3 Ethereum Market
@@ -9,6 +9,46 @@ import {IProposalGenericExecutor} from 'aave-helpers/interfaces/IProposalGeneric
  * - Snapshot: https://signal.aave.com/#/proposal/0x917d0a2c0d9a107d5f8c83b76c291bb34a6a94b85b833b2add96bce7681522ef
  * - Discussion: https://governance.aave.com/t/arfc-stg-onboarding-on-aavev3-ethereum-market/14973
  */
-contract AaveV3_Ethereum_STGOnboardingOnAaveV3EthereumMarket_20231008 is IProposalGenericExecutor {
-  function execute() external {}
+contract AaveV3_Ethereum_STGOnboardingOnAaveV3EthereumMarket_20231008 is AaveV3PayloadEthereum {
+  address public constant STG = 0xaf5191b0de278c7286d6c7cc6ab6bb8a73ba2cd6;
+  address public constant STG_PRICE_FEED = 0x7A9f34a0Aa917D438e9b6E630067062B7F8f6f3d;
+  
+  function newListings() public pure override returns (IEngine.Listing[] memory) {
+    IEngine.Listing[] memory listings = new IEngine.Listing[](1);
+
+    listings[0] = IEngine.Listing({
+      asset: STG,
+      assetSymbol: 'STG',
+      priceFeed: STG_PRICE_FEED,
+      rateStrategyParams: Rates.RateStrategyParams({
+        optimalUsageRatio: _bpsToRay(45_00),
+        baseVariableBorrowRate: 0,
+        variableRateSlope1: _bpsToRay(7_00),
+        variableRateSlope2: _bpsToRay(300_00),
+        stableRateSlope1: _bpsToRay(13_00),
+        stableRateSlope2: _bpsToRay(300_00),
+        baseStableRateOffset: _bpsToRay(3_00),
+        stableRateExcessOffset: _bpsToRay(5_00),
+        optimalStableToTotalDebtRatio: _bpsToRay(20_00)
+      }),
+      enabledToBorrow: EngineFlags.ENABLED,
+      stableRateModeEnabled: EngineFlags.DISABLED,
+      borrowableInIsolation: EngineFlags.DISABLED,
+      withSiloedBorrowing: EngineFlags.DISABLED,
+      flashloanable: EngineFlags.ENABLED,
+      ltv: 35_00,
+      liqThreshold: 40_00,
+      liqBonus: 10_00,
+      reserveFactor: 20_00,
+      supplyCap: 10_000_000,
+      borrowCap: 5_500_000,
+      debtCeiling: 3_000_000,
+      liqProtocolFee: 10_00,
+      eModeCategory: 0
+    });
+
+    return listings;
+  }
+  
+  
 }
