a495ec86539d21ee80690106274fe977bbfbc531
defijesus.eth <defijesus@buzzedbearhideout.com>
2023-10-09 16:00:20 +0100

    uppercase constants

--- src/20230926_AaveV3_Eth_FundGHOLiquidityCommittee/AaveV3_Ethereum_FundGHOLiquidityCommittee_20230926.sol
+++ src/20230926_AaveV3_Eth_FundGHOLiquidityCommittee/AaveV3_Ethereum_FundGHOLiquidityCommittee_20230926.sol
@@ -36,41 +36,41 @@ interface IWETH {
  */
 contract AaveV3_Ethereum_FundGHOLiquidityCommittee_20230926 is IProposalGenericExecutor {
 
-  uint256 public constant daiAmount = 406_000e18;
-  uint256 public constant awethAmount = 5 ether;
-  address public constant daiOracle = AaveV3EthereumAssets.DAI_ORACLE;
-  address public constant ghoOracle = 0x3f12643D3f6f874d39C2a4c9f2Cd6f2DbAC877FC;
-  address public constant milkman = 0x11C76AD590ABDFFCD980afEC9ad951B160F02797;
-  address public constant priceChecker = 0xe80a1C615F75AFF7Ed8F08c9F21f9d00982D666c;
-  address public constant liquidityCommittee = 0x205e795336610f5131Be52F09218AF19f0f3eC60;
+  uint256 public constant DAI_AMOUNT = 406_000e18;
+  uint256 public constant AWETH_AMOUNT = 5 ether;
+  address public constant DAI_ORACLE = AaveV3EthereumAssets.DAI_ORACLE;
+  address public constant GHO_ORACLE = 0x3f12643D3f6f874d39C2a4c9f2Cd6f2DbAC877FC;
+  address public constant MILKMAN = 0x11C76AD590ABDFFCD980afEC9ad951B160F02797;
+  address public constant PRICE_CHECKER = 0xe80a1C615F75AFF7Ed8F08c9F21f9d00982D666c;
+  address public constant LIQUIDITY_COMMITTEE = 0x205e795336610f5131Be52F09218AF19f0f3eC60;
 
   function execute() external {
     AaveSwapper SWAPPER = AaveSwapper(AaveMisc.AAVE_SWAPPER_ETHEREUM);
     
-    AaveV3Ethereum.COLLECTOR.transfer(AaveV3EthereumAssets.DAI_A_TOKEN, address(this), daiAmount);
-    AaveV3Ethereum.COLLECTOR.transfer(AaveV3EthereumAssets.WETH_A_TOKEN, address(this), awethAmount);
+    AaveV3Ethereum.COLLECTOR.transfer(AaveV3EthereumAssets.DAI_A_TOKEN, address(this), DAI_AMOUNT);
+    AaveV3Ethereum.COLLECTOR.transfer(AaveV3EthereumAssets.WETH_A_TOKEN, address(this), AWETH_AMOUNT);
 
     uint256 swapperDaiBalance = 
-      AaveV3Ethereum.POOL.withdraw(AaveV3EthereumAssets.DAI_UNDERLYING, daiAmount, AaveMisc.AAVE_SWAPPER_ETHEREUM);
+      AaveV3Ethereum.POOL.withdraw(AaveV3EthereumAssets.DAI_UNDERLYING, DAI_AMOUNT, AaveMisc.AAVE_SWAPPER_ETHEREUM);
     uint256 wethBalance = 
-      AaveV3Ethereum.POOL.withdraw(AaveV3EthereumAssets.WETH_UNDERLYING, awethAmount, address(this));
+      AaveV3Ethereum.POOL.withdraw(AaveV3EthereumAssets.WETH_UNDERLYING, AWETH_AMOUNT, address(this));
 
     IWETH(AaveV3EthereumAssets.WETH_UNDERLYING).withdraw(wethBalance);
 
     SWAPPER.swap(
-      milkman,
-      priceChecker,
+      MILKMAN,
+      PRICE_CHECKER,
       AaveV3EthereumAssets.DAI_UNDERLYING,
       AaveV3EthereumAssets.GHO_UNDERLYING,
-      daiOracle,
-      ghoOracle,
+      DAI_ORACLE,
+      GHO_ORACLE,
       address(AaveV3Ethereum.COLLECTOR),
       swapperDaiBalance,
       50
     );
 
-    (bool s, ) = liquidityCommittee.call{ value: wethBalance }("");
+    (bool s, ) = LIQUIDITY_COMMITTEE.call{ value: wethBalance }("");
     require(s);
-    AaveV3Ethereum.COLLECTOR.approve(AaveV3EthereumAssets.GHO_UNDERLYING, liquidityCommittee, daiAmount);
+    AaveV3Ethereum.COLLECTOR.approve(AaveV3EthereumAssets.GHO_UNDERLYING, LIQUIDITY_COMMITTEE, DAI_AMOUNT);
   }
 }
