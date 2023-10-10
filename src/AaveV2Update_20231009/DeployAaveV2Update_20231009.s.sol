9c961838ba0b5f788e15beb9685e3a10dd74e8a6
Jonathan Reem <jonathan.reem@gmail.com>
2023-10-10 10:05:05 -0700

    Add deployed payload address.

--- src/AaveV2Update_20231009/DeployAaveV2Update_20231009.s.sol
+++ src/AaveV2Update_20231009/DeployAaveV2Update_20231009.s.sol
@@ -12,7 +12,7 @@ import {
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
-    payloads[0] = GovHelpers.buildMainnet(address(0));
+    payloads[0] = GovHelpers.buildMainnet(0xff374aD1be52fF54Cf576586253a113d3F48D4B7);
     GovHelpers.createProposal(
       payloads,
       GovHelpers.ipfsHashFile(
